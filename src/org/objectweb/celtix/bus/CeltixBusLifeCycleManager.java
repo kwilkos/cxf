f29c7034d5c0275601217a7b08e78cea7e06b5ec
Andrea Smyth <andreasmyth@apache.org>
2005-08-18 15:59:48 +0000

    Forgot to 'add' this file.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@434630 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/org/objectweb/celtix/bus/CeltixBusLifeCycleManager.java
@@ -0,0 +1,52 @@
+package org.objectweb.celtix.bus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.objectweb.celtix.buslifecycle.BusLifeCycleListener;
+import org.objectweb.celtix.buslifecycle.BusLifeCycleManager;
+
+public class CeltixBusLifeCycleManager implements BusLifeCycleManager {
+
+    private List<BusLifeCycleListener> listeners;
+    
+    public CeltixBusLifeCycleManager() {
+        listeners = new ArrayList<BusLifeCycleListener>();
+    }
+    
+    /* (non-Javadoc)
+     * @see org.objectweb.celtix.buslifecycle.BusLifeCycleManager#registerLifeCycleListener(
+     * org.objectweb.celtix.buslifecycle.BusLifeCycleListener)
+     */
+    public void registerLifeCycleListener(BusLifeCycleListener listener) {
+        listeners.add(listener);
+        
+    }
+
+    /* (non-Javadoc)
+     * @see org.objectweb.celtix.buslifecycle.BusLifeCycleManager#unregisterLifeCycleListener(
+     * org.objectweb.celtix.buslifecycle.BusLifeCycleListener)
+     */
+    public void unregisterLifeCycleListener(BusLifeCycleListener listener) {
+        listeners.remove(listener);      
+    }
+    
+    void initComplete() {
+        for (BusLifeCycleListener listener : listeners) {
+            listener.initComplete();
+        }
+    }
+    
+    void preShutdown() {
+        for (BusLifeCycleListener listener : listeners) {
+            listener.preShutdown();
+        }
+    }
+    
+    void postShutdown() {
+        for (BusLifeCycleListener listener : listeners) {
+            listener.postShutdown();
+        }
+    }
+        
+}
