33afea33afc35f23fcdfbe244bd3d438e6c9e7ee
Conrad O'Dea <cjodea@apache.org>
2005-09-07 09:39:30 +0000

    added utility methods for launching server to base test class
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@434680 13f79535-47bb-0310-9956-ffa450edef68

--- test/system/org/objectweb/celtix/systest/basic/ClientServerTest.java
+++ test/system/org/objectweb/celtix/systest/basic/ClientServerTest.java
@@ -1,8 +1,7 @@
 package org.objectweb.celtix.systest.basic;
 
-import java.io.IOException;
+import java.lang.reflect.UndeclaredThrowableException;
 import java.net.URL;
-import java.rmi.RemoteException;
 
 import javax.xml.namespace.QName;
 import javax.xml.ws.Service;
@@ -11,40 +10,27 @@ import javax.xml.ws.ServiceFactory;
 import org.objectweb.celtix.BusException;
 import org.objectweb.celtix.bus.ServiceFactoryImpl;
 import org.objectweb.celtix.systest.common.ClientServerTestBase;
-import org.objectweb.celtix.systest.common.ServerLauncher;
 import org.objectweb.hello_world_soap_http.Greeter;
 
 public class ClientServerTest extends ClientServerTestBase {
     
-    ServerLauncher sl = new ServerLauncher(Server.class.getName());
     
     private QName serviceName = new QName("http://objectweb.org/hello_world_soap_http", "SOAPService");    
     private QName portName = new QName("http://objectweb.org/hello_world_soap_http", "SoapPort");
+
     
-     
     public void setUp() throws BusException {
-        try { 
-            super.setUp();
-            assertTrue("server launch failed", sl.launchServer());
-        } catch (IOException ex) {
-            ex.printStackTrace();
-            fail(ex.toString());
-        }
+        super.setUp();
+        launchServer(Server.class);
     }
     
     public void tearDown() throws BusException {
         super.tearDown();
-        try { 
-            sl.stopServer(); 
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }        
     }
     
     
-    public void testBasicConnection() throws RemoteException {
-        
-        
+    public void testBasicConnection() throws Exception {
+                
         URL wsdl = getClass().getResource("../resource/hello_world.wsdl");
         ServiceFactory factory = ServiceFactory.newInstance();
         assertNotNull(factory);
@@ -52,12 +38,15 @@ public class ClientServerTest extends ClientServerTestBase {
         
         Service service = factory.createService(wsdl, serviceName);
         assertNotNull(service);
-        
-        Greeter greeter = (Greeter) service.getPort(portName, Greeter.class);
-        String greeting = greeter.greetMe("Hello-Milestone");
-        assertNotNull("no response received from service", greeting);
-        System.out.println("response from service: " +  greeting);
-        
+
+        try { 
+            Greeter greeter = (Greeter) service.getPort(portName, Greeter.class);
+            String greeting = greeter.greetMe("Hello-Milestone");
+            assertNotNull("no response received from service", greeting);
+            System.out.println("response from service: " +  greeting);
+        } catch (UndeclaredThrowableException ex) {
+            throw (Exception)ex.getCause();
+        }
     } 
     
     public static void main(String[] args) {
