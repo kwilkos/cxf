176bea29440e1108dc1b930ddc52ff4ccaa79ae9
Peter Jones <peterjones@apache.org>
2005-09-09 22:05:34 +0000

    Added tools.jar to CLASSPATH in celtix_env script.
    Also added some javadoc to some interfaces.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@434702 13f79535-47bb-0310-9956-ffa450edef68

--- src/org/objectweb/celtix/bindings/BindingFactory.java
+++ src/org/objectweb/celtix/bindings/BindingFactory.java
@@ -5,10 +5,32 @@ import javax.xml.ws.Endpoint;
 import org.objectweb.celtix.Bus;
 import org.objectweb.celtix.addressing.EndpointReferenceType;
 
+/**
+ * A factory interface for creating client and server bindings. 
+ */
 public interface BindingFactory {
+    /**
+     * Initialize the factory with a Bus reference.
+     *
+     * @param bus The <code>Bus</code> for this BindingFactory.
+     */
     void init(Bus bus);
     
+    /**
+     * Creates a ClientBinding using the <code>EndpointReferenceType</code>.
+     *
+     * @param reference The EndpointReferenceType the binding will use.
+     * @return ClientBinding The newly created ClientBinding.
+     */
     ClientBinding createClientBinding(EndpointReferenceType reference);
 
+    /**
+     * Creates a ServerBinding for the <code>EndpointReferenceType</code>
+     * and <code>Endpoint</code> provided.
+     *
+     * @param reference The EndpointReferenceType the binding will use.
+     * @param endpoint The server endpoint.
+     * @return ServerBinding The newly created ServerBinding.
+     */
     ServerBinding createServerBinding(EndpointReferenceType reference, Endpoint endpoint);
 }
--- src/org/objectweb/celtix/buslifecycle/BusLifeCycleListener.java
+++ src/org/objectweb/celtix/buslifecycle/BusLifeCycleListener.java
@@ -1,9 +1,31 @@
 package org.objectweb.celtix.buslifecycle;
 
+/**
+ * The listener interface for receiving notification of <code>Bus</code>
+ * lifecycle events.
+ *
+ * A class that implements this interface will have its methods called
+ * when the associated lifecycle events occur.  An implementing class
+ * must register itself with the Bus through the
+ * <code>BusLifeCycleManager</code> interface.
+ */
 public interface BusLifeCycleListener {
 
+    /**
+     * Invoked when the <code>Bus</code> has been initialized.
+     *
+     */
     void initComplete();
     
+    /**
+     * Invoked before the <code>Bus</code> is shutdown.
+     *
+     */
     void preShutdown();
+
+    /**
+     * Invoked after the <code>Bus</code> is shutdown.
+     *
+     */
     void postShutdown();
 }
