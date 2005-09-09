81cf5e5ca28f9527c54aa7adba85a78ed9b4ab98
Conrad O'Dea <cjodea@apache.org>
2005-09-09 15:35:46 +0000

    added env and tool wrapper scripts removed jde project file from demo updated release packaging to include bin directory
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@434695 13f79535-47bb-0310-9956-ffa450edef68

--- samples/hello_world/prj.el
+++
@@ -1,3 +0,0 @@
-(jde-project-file-version "1.0")
-(jde-set-variables
- '(jde-global-classpath (quote ("~/Applications/java/jaxws-2.0ea2/lib/jaxws-api.jar" "../../build/lib/celtix.jar" "./build/classes"))))
