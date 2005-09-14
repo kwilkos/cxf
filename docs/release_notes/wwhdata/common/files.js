e87ccfdbedc760371abd5ef99d786c61fcb49bc8
J. Daniel Kulp <dkulp@apache.org>
2005-09-14 13:50:57 +0000

    Add release notes
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@434709 13f79535-47bb-0310-9956-ffa450edef68

---
+++ docs/release_notes/wwhdata/common/files.js
@@ -0,0 +1,9 @@
+function  WWHBookData_Files(P)
+{
+P.fA("Milestone 1 Installation and Release Notes","celtix_relnotes1.html");
+P.fA("Features in Celtix Milestone 1","celtix_relnotes2.html");
+P.fA("Installation Prerequisites","celtix_relnotes3.html");
+P.fA("Installing Celtix","celtix_relnotes4.html");
+P.fA("Testing the Installation","celtix_relnotes5.html");
+P.fA("Reporting Problems","celtix_relnotes6.html");
+}
---
+++ docs/release_notes/wwhelp/wwhimpl/java/private/options.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2001-2003 Quadralay Corporation.  All rights reserved.
+//
+
+function  WWHJavaSettings_Object()
+{
+}
