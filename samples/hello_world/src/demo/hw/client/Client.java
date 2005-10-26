bea07eaaef69aefc413a7e3a4e69bd8f28e2e214
Ajay Paibir <ajaypaibir@apache.org>
2005-10-26 11:37:10 +0000

    Added a wsdl fault operation to the existing Hello World demo to demonstrate support for wsdl faults.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@434794 13f79535-47bb-0310-9956-ffa450edef68

--- samples/hello_world/src/demo/hw/client/Client.java
+++ samples/hello_world/src/demo/hw/client/Client.java
@@ -7,46 +7,48 @@ import javax.xml.ws.WebServiceException;
 import org.objectweb.celtix.Bus;
 import org.objectweb.celtix.BusException;
 import org.objectweb.hello_world_soap_http.Greeter;
+import org.objectweb.hello_world_soap_http.PingMeFault;
 import org.objectweb.hello_world_soap_http.SOAPService;
 import java.rmi.RemoteException;
 
 public class Client {
 
-      static QName serviceName = new QName("http://objectweb.org/hello_world_soap_http", 
+    static QName serviceName = new QName("http://objectweb.org/hello_world_soap_http", 
                                            "SOAPService");
 
-      static QName portName = new QName("http://objectweb.org/hello_world_soap_http", 
+    static QName portName = new QName("http://objectweb.org/hello_world_soap_http", 
                                         "SoapPort");
-      static Bus bus; 
+    static Bus bus; 
 
-	public static void main(String[] args) {
-		
-            
-		try { 
-			if (args.length == 0) {
-				System.err.println("please provide wsdl");
-				System.exit(0);
-			}
-			File wsdl = new File(args[0]);
-			
-                        bus = Bus.init();
-			
-			SOAPService service = new SOAPService(wsdl.toURL(), serviceName);
-                        Greeter greeter = (Greeter) service.getPort(portName, Greeter.class);
-                        String greeting = greeter.greetMe("blah");
-                        System.out.println("response from service: " +  greeting);
-                        
-		} catch(Exception ex) {
-                    System.out.println("error during remote invocation: " );
-                    ex.printStackTrace();
-		} finally { 
-                    if (bus != null) { 
-                        try {
-                            bus.shutdown(true);
-                        } catch(BusException busex) {
-                        }
-                    }
-                }
+    public static void main(String[] args) throws Exception {
+        
+        if (args.length == 0) {
+            System.err.println("please provide wsdl");
+            System.exit(0);
         }
+
+        File wsdl = new File(args[0]);
+
+        bus = Bus.init();
+
+        SOAPService service = new SOAPService(wsdl.toURL(), serviceName);
+        Greeter greeter = (Greeter) service.getPort(portName, Greeter.class);
+        String greeting = greeter.greetMe("blah");
+        System.out.println("response from greetMe operatione: " +  greeting);
+        
+        String hiReply = greeter.sayHi();
+        System.out.println("response from sayHi operation: " +  hiReply);
+
+        try {
+            greeter.pingMe();
+        } catch(PingMeFault ex) {
+            System.out.println("Exception: PingMeFault has Occurred.");
+            System.out.println(ex.toString());
+        }
+                        
+        if (bus != null) { 
+            bus.shutdown(true);
+        }
+    }
 }
 
--- samples/hello_world/src/demo/hw/server/GreeterImpl.java
+++ samples/hello_world/src/demo/hw/server/GreeterImpl.java
@@ -3,6 +3,8 @@ package demo.hw.server;
 import java.rmi.RemoteException;
 import java.util.logging.Logger;
 import org.objectweb.hello_world_soap_http.Greeter;
+import org.objectweb.hello_world_soap_http.PingMeFault;
+import org.objectweb.hello_world_soap_http.types.FaultDetail;
 
 @javax.jws.WebService(name = "Greeter", serviceName = "SOAPService", 
                       targetNamespace = "http://objectweb.org/hello_world_soap_http", 
@@ -28,4 +30,14 @@ public class GreeterImpl implements Greeter {
         logger.info("Executing operation greetMe");
         return "Bonjour";
     }
+    
+    public void pingMe() throws PingMeFault {
+        FaultDetail faultDetail = new FaultDetail();
+        faultDetail.setMajor((short)2);
+        faultDetail.setMinor((short)1);
+        logger.info("Executing operation pingMe, throwing PingMeFault exception");
+        throw new PingMeFault("PingMeFault", faultDetail);
+    }
+
+    
 }
