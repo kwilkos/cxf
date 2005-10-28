e9eac306c7d7229ba39e37657a6020805a8fbc1e
Conrad O'Dea <cjodea@apache.org>
2005-10-28 14:51:27 +0000

    handler demo
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@434822 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/handlers/README.txt
@@ -0,0 +1,16 @@
+
+This demo shows how the JAXWS handlers can be used.  The server
+registers a SOAP protocol handler which simply logs incoming and
+outgoing messages to the console.  
+
+On the server side the handlers registered using the @HandlerChain
+annotation on the service implementation class.
+
+Running the demo
+---------------
+
+The ant build script can be used to build and run the demo.  The
+server and client targets automatically build the demo.
+
+* ant server 
+* ant client 
---
+++ samples/handlers/src/demo/handlers/server/Server.java
@@ -0,0 +1,53 @@
+package demo.handlers.server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.ws.Endpoint;
+import javax.xml.ws.handler.Handler;
+
+import org.objectweb.celtix.Bus;
+import org.objectweb.celtix.BusException;
+
+import demo.handlers.common.LoggingHandler;
+
+public class Server implements Runnable {
+
+    Bus bus;
+
+    protected Server(String[] args) throws Exception {
+        System.out.println("Starting AddNumbers Server");
+
+        /**
+         * Creation of the endpoint could be part of the bus initialisation
+         * based on configuration. For now, do it manually.
+         */
+
+        bus = Bus.init(args);
+        Object implementor = new AddNumbersImpl();
+        String address = "http://localhost:9000/handlers/AddNumbersService/AddNumbersPort";
+        Endpoint ep = Endpoint.publish(address, implementor);
+
+        System.out.println("Registring Logging Handler...");
+        List<Handler> hc = new ArrayList<Handler>();
+        hc.add(new LoggingHandler());
+        ep.getBinding().setHandlerChain(hc);
+    }
+
+    public static void main(String args[]) throws Exception {
+        Server server = new Server(args);
+        server.run();
+    }
+
+    public void run() {
+        System.out.println("Running Celtix Bus...");
+
+        bus.run();
+    }
+
+    void shutdown(boolean wait) throws BusException {
+        bus.shutdown(wait);
+    }
+
+
+}
