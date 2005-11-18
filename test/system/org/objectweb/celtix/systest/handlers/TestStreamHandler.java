4b3fb7675a507ae8ea05d97eae7564324401b816
Conrad O'Dea <cjodea@apache.org>
2005-11-18 18:45:39 +0000

    adding missing test handler class
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@434915 13f79535-47bb-0310-9956-ffa450edef68

---
+++ test/system/org/objectweb/celtix/systest/handlers/TestStreamHandler.java
@@ -0,0 +1,102 @@
+package org.objectweb.celtix.systest.handlers;
+
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+import javax.xml.ws.ProtocolException;
+import javax.xml.ws.handler.MessageContext;
+import org.objectweb.celtix.context.StreamMessageContext;
+import org.objectweb.celtix.handlers.StreamHandler;
+
+public class  TestStreamHandler extends TestHandlerBase 
+    implements StreamHandler {
+
+    public TestStreamHandler() {
+        this(true); 
+    } 
+
+    public TestStreamHandler(boolean serverSide) {
+        super(serverSide);
+    }
+
+    public String getHandlerId() { 
+        return "streamHandler" + getId();
+    }
+    
+    public final boolean handleMessage(StreamMessageContext ctx) {
+
+        methodCalled("handleMessage"); 
+
+        if (isServerSideHandler()) { 
+            if (!isOutbound(ctx)) { 
+                getHandlerInfoList(ctx).add(getHandlerId());
+            } else { 
+                // compress outbound on server side
+                setupCompressionOutputStream(ctx);
+            } 
+        } else {  
+            if (!isOutbound(ctx)) { 
+                // decompress inbound on client side
+                setupDecompressionInputStream(ctx); 
+            } 
+        } 
+        return true;
+    }
+
+
+    public final boolean handleFault(StreamMessageContext ctx) {
+        methodCalled("handleFault"); 
+        return true;
+    }
+
+    public final void init(final Map map) {
+        methodCalled("init"); 
+    }
+
+    public final void destroy() {
+        methodCalled("destroy"); 
+    }
+
+    public final void close(MessageContext messageContext) {
+        methodCalled("close"); 
+    }
+
+
+    public String toString() { 
+        return getHandlerId();
+    } 
+
+    private void setupDecompressionInputStream(StreamMessageContext ctx) { 
+        try { 
+            
+            GZIPInputStream zipIn = new GZIPInputStream(ctx.getInputStream());
+            ctx.setInputStream(zipIn); 
+        } catch (IOException ex) { 
+            throw new ProtocolException(ex);
+        }
+    } 
+
+    private void setupCompressionOutputStream(StreamMessageContext ctx) { 
+
+        try { 
+            final FileOutputStream log = new FileOutputStream("/tmp.msg.log");
+            GZIPOutputStream zipOut = new GZIPOutputStream(ctx.getOutputStream()) {
+                    public void flush() throws IOException { 
+                        super.finish();
+                        super.flush();
+                    } 
+                };
+
+            ctx.setOutputStream(zipOut); 
+        } catch (IOException ex) { 
+            throw new ProtocolException(ex);
+        }
+    } 
+
+    private boolean isOutbound(MessageContext ctx) {
+        return (Boolean)ctx.get(MessageContext.MESSAGE_OUTBOUND_PROPERTY);
+    }
+}
