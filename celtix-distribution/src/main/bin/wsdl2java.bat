ac70c88ebb3f07e28882517392201cfc1079e28e
J. Daniel Kulp <dkulp@apache.org>
2006-01-09 22:56:03 +0000

    Get tools working in bin kits
    * Create a celtix.jar manifest jar (hack alert)
    * Update the tools to work with the new bin format (still don't work in src distro)
    
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@435247 13f79535-47bb-0310-9956-ffa450edef68

--- celtix-distribution/src/main/bin/wsdl2java.bat
+++ celtix-distribution/src/main/bin/wsdl2java.bat
@@ -6,13 +6,6 @@ rem
 
 set CELTIX_HOME=%~dp0..
 
-if not defined JAXWS_HOME (
-    set JAXWS_HOME=%CELTIX_HOME%\lib\jaxws-ri\20051104
-)
-if not exist %JAXWS_HOME% (
-    set JAXWS_HOME=%CELTIX_HOME%\tools\jaxws-ri\20051104
-)
-
 if not defined JAVA_HOME goto no_java_home
 
 set CLASSPATH=%JAVA_HOME%\lib\tools.jar;%CLASSPATH%
@@ -28,7 +21,7 @@ if exist %CELTIX_HOME%\build\lib\celtix.jar (
 
 IF "%1"=="-celtix" GOTO CELTIX_TOOL
 
-"%JAVA_HOME%\bin\java" -Djaxws.home="%JAXWS_HOME%" -Djava.util.logging.config.file="%CELTIX_HOME%\etc\logging.properties" org.objectweb.celtix.tools.Wsdl2Java %*
+"%JAVA_HOME%\bin\java" -Djaxws.home="%CELTIX_HOME%" -Djava.util.logging.config.file="%CELTIX_HOME%\etc\logging.properties" org.objectweb.celtix.tools.Wsdl2Java %*
 GOTO END
 
 :CELTIX_TOOL
