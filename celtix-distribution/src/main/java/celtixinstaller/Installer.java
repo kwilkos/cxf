77b53060ef01e542c69d0e417089efa21644f5a5
J. Daniel Kulp <dkulp@apache.org>
2006-01-11 21:09:53 +0000

    JAR based packaging
    * Change from tar.gz/zip to jar for packaging
    * Simple installer to unpack the jar
    * Installer will convert text files to native form
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@435264 13f79535-47bb-0310-9956-ffa450edef68

---
+++ celtix-distribution/src/main/java/celtixinstaller/Installer.java
@@ -0,0 +1,115 @@
+package celtixinstaller;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+
+public final class Installer {
+    static final Set<String> BINARY_EXTS = new TreeSet<String>();
+    static {
+        BINARY_EXTS.add("jar");
+        BINARY_EXTS.add("zip");
+        BINARY_EXTS.add("gif");
+        BINARY_EXTS.add("jpg");
+        BINARY_EXTS.add("jpeg");
+        BINARY_EXTS.add("pdf");
+        BINARY_EXTS.add("png");
+        BINARY_EXTS.add("odt");
+        BINARY_EXTS.add("ott");
+        BINARY_EXTS.add("xsd");
+    }
+
+    private Installer() {
+        //never constructed
+    }
+
+    private static boolean isBinary(String s) {
+        if (s.indexOf("maven_repo") != -1) {
+            return true;
+        }
+        for (String ext : BINARY_EXTS) {
+            if (s.endsWith(ext)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String args[]) throws Exception {
+        File outputDir = new File(".");
+        outputDir = outputDir.getCanonicalFile();
+
+        if (args.length != 0) {
+            outputDir = new File(args[0]);
+        }
+
+        System.out.println("Unpacking celtix to " + outputDir.toString());
+
+
+        URL url = Installer.class.getResource("/celtixinstaller/Installer.class");
+        String jarf = url.getFile();
+        jarf = jarf.substring(0, jarf.indexOf("!"));
+        url = new URL(jarf);
+
+        byte buffer[] = new byte[4096];
+        JarInputStream jin = new JarInputStream(new FileInputStream(url.getFile()));
+        List<String> executes = new ArrayList<String>();
+
+
+        for (JarEntry entry = jin.getNextJarEntry(); entry != null; entry = jin.getNextJarEntry()) {
+            if (entry.isDirectory()) {
+                if (!entry.getName().startsWith("META-INF") 
+                    && !entry.getName().startsWith("celtixinstaller")) {
+                    File file = new File(outputDir, entry.getName());
+                    file.mkdirs();
+                }
+            } else if (!entry.getName().startsWith("META-INF")
+                       && !entry.getName().startsWith("celtixinstaller")) {
+
+                boolean binary = isBinary(entry.getName().toLowerCase());
+                if (entry.getName().indexOf("/bin/") != -1
+                    || entry.getName().indexOf("\\bin\\") != -1) {
+                    executes.add(entry.getName());
+                }
+
+                if (binary) {
+                    OutputStream out = new FileOutputStream(new File(outputDir, entry.getName()));
+                    for (int len = jin.read(buffer); len != -1; len = jin.read(buffer)) {
+                        out.write(buffer, 0, len);
+                    }
+                    out.close();
+                } else {
+                    BufferedWriter writer = new BufferedWriter(
+                        new FileWriter(new File(outputDir, entry.getName())));
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(jin));
+                    for (String s = reader.readLine(); s != null; s = reader.readLine()) {
+                        writer.write(s);
+                        writer.newLine();
+                    }
+                    writer.close();
+                }
+            }
+        }
+
+        if (System.getProperty("os.name").indexOf("Windows") == -1
+            && !executes.isEmpty()) {
+            //add executable bit
+            executes.add(0, "chmod");
+            executes.add(1, "+x");
+
+            Runtime.getRuntime().exec(executes.toArray(new String[executes.size()]));
+        }
+    }
+}
