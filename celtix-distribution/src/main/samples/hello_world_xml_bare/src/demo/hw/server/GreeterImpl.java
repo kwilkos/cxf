ccabd90adbbefc653f5e98de4ec120f37eca229e
James Maode Mao <mmao@apache.org>
2006-03-08 13:04:47 +0000

    Fixed the code style for xml binding demos
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@435497 13f79535-47bb-0310-9956-ffa450edef68

--- celtix-distribution/src/main/samples/hello_world_xml_bare/src/demo/hw/server/GreeterImpl.java
+++ celtix-distribution/src/main/samples/hello_world_xml_bare/src/demo/hw/server/GreeterImpl.java
@@ -7,28 +7,28 @@ import org.objectweb.hello_world_xml_http.bare.types.MyComplexStruct;
 @javax.jws.WebService(name = "Greeter", serviceName = "XMLService", 
                       targetNamespace = "http://objectweb.org/hello_world_xml_http/bare", 
                       wsdlLocation = "file:./wsdl/hello_world.wsdl")
-    public class GreeterImpl implements Greeter {
+public class GreeterImpl implements Greeter {
 
-        private static final Logger LOG = Logger.getLogger(GreeterImpl.class.getPackage().getName());
-
-        public String greetMe(String me) {
-            LOG.info("Executing operation greetMe");
-            System.out.println("Executing operation greetMe");
-            System.out.println("Message received: " + me + "\n");
-            return "Hello " + me;
-        }
-
-        public String sayHi() {
-            LOG.info("Executing operation sayHi");
-            System.out.println("Executing operation sayHi" + "\n");
-            return "Bonjour";
-        }
-
-        public MyComplexStruct sendReceiveData(MyComplexStruct in) {
-            LOG.info("Executing operation sendReceiveData");
-            System.out.println("Executing operation sendReceiveData");
-            System.out.println("Received struct with values :\nElement-1 : " + in.getElem1() + "\nElement-2 : "
-                               + in.getElem2() + "\nElement-3 : " + in.getElem3() + "\n");
-            return in;
-        }
+    private static final Logger LOG = Logger.getLogger(GreeterImpl.class.getPackage().getName());
+    
+    public String greetMe(String me) {
+        LOG.info("Executing operation greetMe");
+        System.out.println("Executing operation greetMe");
+        System.out.println("Message received: " + me + "\n");
+        return "Hello " + me;
     }
+    
+    public String sayHi() {
+        LOG.info("Executing operation sayHi");
+        System.out.println("Executing operation sayHi" + "\n");
+        return "Bonjour";
+    }
+    
+    public MyComplexStruct sendReceiveData(MyComplexStruct in) {
+        LOG.info("Executing operation sendReceiveData");
+        System.out.println("Executing operation sendReceiveData");
+        System.out.println("Received struct with values :\nElement-1 : " + in.getElem1() + "\nElement-2 : "
+                           + in.getElem2() + "\nElement-3 : " + in.getElem3() + "\n");
+        return in;
+    }
+}
--- celtix-distribution/src/main/samples/hello_world_xml_wrapped/src/demo/hw/server/GreeterImpl.java
+++ celtix-distribution/src/main/samples/hello_world_xml_wrapped/src/demo/hw/server/GreeterImpl.java
@@ -7,36 +7,36 @@ import org.objectweb.hello_world_xml_http.wrapped.Greeter;
                       targetNamespace = "http://objectweb.org/hello_world_xml_http/wrapped", 
                       wsdlLocation = "file:./wsdl/hello_world.wsdl")
                   
-    public class GreeterImpl implements Greeter {
-
-        private static final Logger LOG = 
-            Logger.getLogger(GreeterImpl.class.getPackage().getName());
+public class GreeterImpl implements Greeter {
     
-        /* (non-Javadoc)
-         * @see org.objectweb.hello_world_xml_http.wrapped.Greeter#greetMe(java.lang.String)
-         */
-        public String greetMe(String me) {
-            LOG.info("Executing operation greetMe");
-            System.out.println("Executing operation greetMe");
-            System.out.println("Message received: " + me + "\n");
-            return "Hello " + me;
-        }
+    private static final Logger LOG = 
+        Logger.getLogger(GreeterImpl.class.getPackage().getName());
     
-        /* (non-Javadoc)
-         * @see org.objectweb.hello_world_xml_http.wrapped.Greeter#greetMeOneWay(java.lang.String)
-         */
-        public void greetMeOneWay(String me) {
-            LOG.info("Executing operation greetMeOneWay");
-            System.out.println("Executing operation greetMeOneWay\n");
-            System.out.println("Hello there " + me);
-        }
-
-        /* (non-Javadoc)
-         * @see org.objectweb.hello_world_xml_http.wrapped.Greeter#sayHi()
-         */
-        public String sayHi() {
-            LOG.info("Executing operation sayHi");
-            System.out.println("Executing operation sayHi\n");
-            return "Bonjour";
-        }   
+    /* (non-Javadoc)
+     * @see org.objectweb.hello_world_xml_http.wrapped.Greeter#greetMe(java.lang.String)
+     */
+    public String greetMe(String me) {
+        LOG.info("Executing operation greetMe");
+        System.out.println("Executing operation greetMe");
+        System.out.println("Message received: " + me + "\n");
+        return "Hello " + me;
     }
+    
+    /* (non-Javadoc)
+     * @see org.objectweb.hello_world_xml_http.wrapped.Greeter#greetMeOneWay(java.lang.String)
+     */
+    public void greetMeOneWay(String me) {
+        LOG.info("Executing operation greetMeOneWay");
+        System.out.println("Executing operation greetMeOneWay\n");
+        System.out.println("Hello there " + me);
+    }
+    
+    /* (non-Javadoc)
+     * @see org.objectweb.hello_world_xml_http.wrapped.Greeter#sayHi()
+     */
+    public String sayHi() {
+        LOG.info("Executing operation sayHi");
+        System.out.println("Executing operation sayHi\n");
+        return "Bonjour";
+    }   
+}
