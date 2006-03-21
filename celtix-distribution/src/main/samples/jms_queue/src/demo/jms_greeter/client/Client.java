63cb77847e0ea94e2b9f0262cb6bca7cb708fb15
Ulhas Bhole <ulhasbhole@apache.org>
2006-03-21 16:40:14 +0000

    * Updated JMS queue sample to demonstrate JMS Context usage.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@435577 13f79535-47bb-0310-9956-ffa450edef68

--- celtix-distribution/src/main/samples/jms_queue/src/demo/jms_greeter/client/Client.java
+++ celtix-distribution/src/main/samples/jms_queue/src/demo/jms_greeter/client/Client.java
@@ -1,9 +1,18 @@
 package demo.jms_greeter.client;
 
 import java.io.File;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.xml.namespace.QName;
+import javax.xml.ws.BindingProvider;
 import org.objectweb.celtix.jms_greeter.JMSGreeterPortType;
 import org.objectweb.celtix.jms_greeter.JMSGreeterService;
+import org.objectweb.celtix.transports.jms.context.JMSMessageHeadersType;
+import org.objectweb.celtix.transports.jms.context.JMSPropertyType;
+
 
 public final class Client {
 
@@ -34,10 +43,60 @@ public final class Client {
         System.out.println("Invoking greetMe...");
         System.out.println("server responded with: " + greeter.greetMe(System.getProperty("user.name")));
         System.out.println();
-        
+
         System.out.println("Invoking greetMeOneWay...");
         greeter.greetMeOneWay(System.getProperty("user.name"));
         System.out.println("No response from server as method is OneWay");
+        System.out.println();
+
+        // Demonstration of JMS Context usage
+
+        InvocationHandler handler = Proxy.getInvocationHandler(greeter);
+
+        BindingProvider  bp = null;
+
+        if (handler instanceof BindingProvider) {
+            bp = (BindingProvider) handler;
+            Map<String, Object> requestContext = bp.getRequestContext();
+            JMSMessageHeadersType requestHeader = new JMSMessageHeadersType();
+            requestHeader.setJMSCorrelationID("JMS_QUEUE_SAMPLE_CORRELATION_ID");
+            requestHeader.setJMSExpiration(3600000L);
+            JMSPropertyType propType = new JMSPropertyType();
+            propType.setName("Test.Prop");
+            propType.setValue("mustReturn");
+            requestHeader.getProperty().add(propType);
+            requestContext.put("org.objectweb.celtix.jms.client.request.headers", requestHeader);
+            //To override the default receive timeout.
+            requestContext.put("org.objectweb.celtix.jms.client.timeout", new Long(10));
+        }
+
+        System.out.println("Invoking sayHi with JMS Context information ...");
+        System.out.println("server responded with: " + greeter.sayHi());
+
+        if (bp != null) {
+            Map<String, Object> responseContext = bp.getResponseContext();
+            JMSMessageHeadersType responseHdr =
+                 (JMSMessageHeadersType) responseContext.get("org.objectweb.celtix.jms.client.response.headers");
+            if (responseHdr == null) {
+                System.out.println("response Header should not be null");
+                System.out.println();
+                System.exit(1);
+            }
+
+            if ("JMS_QUEUE_SAMPLE_CORRELATION_ID".equals(responseHdr.getJMSCorrelationID()) &&
+                responseHdr.getProperty() != null) {
+                System.out.println("Received expected contents in response context");
+            } else {
+                System.out.println("Received wrong contents in response context");
+                System.out.println();
+                System.exit(2);
+            }
+        } else {
+            System.out.println("Failed to get the binding provider cannot access context info.");
+            System.exit(3);
+        }
+
+
         System.out.println();
 
         System.exit(0);
