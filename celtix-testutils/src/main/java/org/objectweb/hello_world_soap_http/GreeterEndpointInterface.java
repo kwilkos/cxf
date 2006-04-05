a342305ab77a1fae7530564ee61f32667031bd90
Sean O'Callaghan <seanoc@apache.org>
2006-04-05 13:56:36 +0000

    remove hardcoded path
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@435709 13f79535-47bb-0310-9956-ffa450edef68

--- celtix-testutils/src/main/java/org/objectweb/hello_world_soap_http/GreeterEndpointInterface.java
+++ celtix-testutils/src/main/java/org/objectweb/hello_world_soap_http/GreeterEndpointInterface.java
@@ -4,7 +4,7 @@ package org.objectweb.hello_world_soap_http;
                       serviceName = "SOAPService",
                       targetNamespace = "http://objectweb.org/hello_world_soap_http", 
                       wsdlLocation = 
-                          "C:/Celtix/trunk/celtix-testutils/src/main/resources/wsdl/hello_world.wsdl")
+                          "resources/wsdl/hello_world.wsdl")
 public interface GreeterEndpointInterface {
 
 }
