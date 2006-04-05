2cbb74daa45588890e5d0cae3d421de55fa08993
Sean O'Callaghan <seanoc@apache.org>
2006-04-05 14:21:45 +0000

    fix build failure
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@435710 13f79535-47bb-0310-9956-ffa450edef68

--- celtix-testutils/src/main/java/org/objectweb/hello_world_soap_http/GreeterEndpointInterface.java
+++ celtix-testutils/src/main/java/org/objectweb/hello_world_soap_http/GreeterEndpointInterface.java
@@ -4,7 +4,7 @@ package org.objectweb.hello_world_soap_http;
                       serviceName = "SOAPService",
                       targetNamespace = "http://objectweb.org/hello_world_soap_http", 
                       wsdlLocation = 
-                          "resources/wsdl/hello_world.wsdl")
+                          "../celtix-testutils/src/main/resources/wsdl/hello_world.wsdl")
 public interface GreeterEndpointInterface {
 
 }
