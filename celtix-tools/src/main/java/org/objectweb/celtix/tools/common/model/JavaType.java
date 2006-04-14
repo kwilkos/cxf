9bc9eb8483a9955cbcf863d27bc8fa9340aef94a
James Maode Mao <mmao@apache.org>
2006-04-14 11:05:14 +0000

    Fixed bug304979 and beautify the client template.
    http://forge.objectweb.org/tracker/index.php?func=detail&aid=304979&goup_id=192&atid=350241
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@435751 13f79535-47bb-0310-9956-ffa450edef68

--- celtix-tools/src/main/java/org/objectweb/celtix/tools/common/model/JavaType.java
+++ celtix-tools/src/main/java/org/objectweb/celtix/tools/common/model/JavaType.java
@@ -1,11 +1,36 @@
 package org.objectweb.celtix.tools.common.model;
 
+import java.lang.reflect.Constructor;
+import java.util.*;
 import javax.xml.namespace.QName;
 import com.sun.xml.bind.api.TypeReference;
 
 public class JavaType {
     
     public static enum Style { IN, OUT, INOUT }
+    private static Map<String, String> typeMapping = new HashMap<String, String>();
+
+    static {
+        typeMapping.put("boolean", "false");
+        typeMapping.put("int", "0");
+        typeMapping.put("long", "0");
+        typeMapping.put("short", "Short.parseShort(\"0\")");
+        typeMapping.put("byte", "Byte.parseByte(\"0\")");
+        typeMapping.put("float", "0.0f");
+        typeMapping.put("double", "0.0");
+        typeMapping.put("char", "0");
+        
+        typeMapping.put("javax.xml.namespace.QName", "new javax.xml.namespace.QName(\"\", \"\")");
+        typeMapping.put("java.net.URI", "new java.net.URI(\"\")");
+
+        typeMapping.put("java.math.BigInteger", "new java.math.BigInteger(\"0\")");
+        typeMapping.put("java.math.BigDecimal", "new java.math.BigDecimal(\"0\")");
+        typeMapping.put("javax.xml.datatype.XMLGregorianCalendar", "null");
+        // javax.xml.datatype.DatatypeFactory.newInstance().newXMLGregorianCalendar()
+        typeMapping.put("javax.xml.datatype.Duration", "null");
+        // javax.xml.datatype.DatatypeFactory.newInstance().newDuration(\"P1Y35DT60M60.500S\")
+    }
+
     protected String name;
     protected String type;
     protected String className;
@@ -39,6 +64,38 @@ public class JavaType {
 
     public String getClassName() {
         return this.className;
+    }
+
+    public String getDefaultTypeValue() {
+        if (this.className.trim().endsWith("[]")) {
+            return "new " + this.className.substring(0, this.className.length() - 2) + "[0]";
+        }
+        if (typeMapping.containsKey(this.className.trim())) {
+            return typeMapping.get(this.className);
+        }
+
+        try {
+            if (hasDefaultConstructor(Class.forName(this.className))) {
+                return "new " + this.className + "()";
+            }
+        } catch (ClassNotFoundException e) {
+            // DONE
+        }
+        return "null";
+    }
+
+    private boolean hasDefaultConstructor(Class clz) {
+        Constructor[] cons = clz.getConstructors();
+        if (cons.length == 0) {
+            return false;
+        } else {
+            for (int i = 0; i < cons.length; i++) {
+                if (cons[i].getParameterTypes().length == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
     
     public void setTargetNamespace(String tns) {
