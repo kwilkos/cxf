929daeea76ffe829da0fc2e7cf9cf1a494020e0b
J. Daniel Kulp <dkulp@apache.org>
2006-04-25 16:42:19 +0000

    Add Steve's javascipt stuff
    * http://forge.objectweb.org/tracker/?func=detail&atid=350241&aid=305056&group_id=192
    * Add the javascript frontend stuff that Steve Vinoski has submitted
    * Still need to figure out the packaging and demos for it as well as javascript client stuff
    
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@435814 13f79535-47bb-0310-9956-ffa450edef68

---
+++ celtix-systests/src/test/java/org/objectweb/celtix/systest/js/resources/hello_world.jsx
@@ -0,0 +1,21 @@
+var WebServiceProvider = {
+    'wsdlLocation': 'file:../celtix-testutils/target/classes/wsdl/hello_world.wsdl',
+    'serviceName': 'SOAPServiceTest1',
+    'portName': 'SoapPort_Test1',
+    'targetNamespace': 'http://objectweb.org/hello_world_soap_http',
+};
+
+var ns = new Namespace('ns', "http://objectweb.org/hello_world_soap_http/types");
+
+WebServiceProvider.invoke = function(req) {
+    default xml namespace = ns;
+    var resp;
+    if (req.localName() == 'greetMe') {
+        resp = <ns:greetMeResponse xmlns:ns={ns}/>;
+        resp.ns::responseType = 'TestGreetMeResponse';
+    } else {
+        resp = <ns:sayHiResponse xmlns:ns={ns}/>;
+        resp.ns::responseType = 'TestSayHiResponse';
+    }
+    return resp;
+}
