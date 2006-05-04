a8470ff6e22245854fed1741f65d14cb4162b021
J. Daniel Kulp <dkulp@apache.org>
2006-05-04 13:56:06 +0000

    Minor javascript patch from Vinoski for spelling mistakes and minor cleanup
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@435888 13f79535-47bb-0310-9956-ffa450edef68

--- celtix-distribution/src/main/samples/js_provider/impl.jsx
+++ celtix-distribution/src/main/samples/js_provider/impl.jsx
@@ -11,12 +11,12 @@ var xs = new Namespace('xs', 'http://www.w3.org/2001/XMLSchema');
 var xsi = new Namespace('xsi', 'http://www.w3.org/2001/XMLSchema-instance');
 var ns = new Namespace('ns', 'http://objectweb.org/hello_world_soap_http/types');
 
+var soapMsg = <SOAP-ENV:Envelope xmlns:SOAP-ENV={SOAP_ENV} xmlns:xs={xs} xmlns:xsi={xsi}><SOAP-ENV:Body/></SOAP-ENV:Envelope>;
+
 WebServiceProvider1.invoke = function(req) {
-    default xml namespace = ns;
-    var name = (req..requestType)[0];
+    var resp = soapMsg;
+    var name = (req..ns::requestType)[0];
     default xml namespace = SOAP_ENV;
-    var resp = <SOAP-ENV:Envelope xmlns:SOAP-ENV={SOAP_ENV} xmlns:xs={xs} xmlns:xsi={xsi}/>;
-    resp.Body = <Body/>;
     resp.Body.ns::greetMeResponse = <ns:greetMeResponse xmlns:ns={ns}/>;
     resp.Body.ns::greetMeResponse.ns::responseType = 'Hi ' + name;
     return resp;
