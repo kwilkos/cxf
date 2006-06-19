8cda5b1c30b33734c941ba7be88d7787898e26d1
Freeman Yue Fang <ffang@apache.org>
2006-06-19 11:06:59 +0000

    add external consumer and external provider using different transport demo for JBI integration
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@436079 13f79535-47bb-0310-9956-ffa450edef68

---
+++ celtix-distribution/src/main/samples/integration/JBI/external_provider_external_consumer/service-unit/src/test/provider/HelloWorldProvider.java
@@ -0,0 +1,50 @@
+package test.provider;
+
+import org.objectweb.hello_world.Greeter;
+import org.objectweb.hello_world.PingMeFault;
+import org.objectweb.hello_world.types.FaultDetail;
+
+
+
+@javax.jws.WebService(name = "Greeter", serviceName = "JMSGreeterService", 
+                      targetNamespace = "http://objectweb.org/hello_world", 
+                      wsdlLocation = "./wsdl/hello_world.wsdl")
+                  
+public class HelloWorldProvider implements Greeter {
+
+
+     /* (non-Javadoc)
+     * @see org.objectweb.hello_world_soap_http.Greeter#greetMe(java.lang.String)
+     */
+    public String greetMe(String me) {
+        System.out.println("Executing operation greetMe");
+        System.out.println("Message received: " + me + "\n");
+        return "Hello " + me;
+    }
+ 
+    /* (non-Javadoc)
+     * @see org.objectweb.hello_world_soap_http.Greeter#greetMeOneWay(java.lang.String)
+     */
+    public void greetMeOneWay(String me) {
+        System.out.println("Executing operation greetMeOneWay\n");
+        System.out.println("Hello there " + me);
+    }
+ 
+    /* (non-Javadoc)
+     * @see org.objectweb.hello_world_soap_http.Greeter#sayHi()
+     */
+    public String sayHi() {
+        System.out.println("Executing operation sayHi\n");
+        return "Bonjour";
+    }
+ 
+    public void pingMe() throws PingMeFault {
+        FaultDetail faultDetail = new FaultDetail();
+        faultDetail.setMajor((short)2);
+        faultDetail.setMinor((short)1);
+        System.out.println("Executing operation pingMe, throwing PingMeFault exception\n");
+        throw new PingMeFault("PingMeFault raised by server", faultDetail);
+    }
+
+
+}
