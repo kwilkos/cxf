0cdde11423d5f5e83ebc2833d8241c5b62869e8c
James Maode Mao <mmao@apache.org>
2006-07-11 03:27:16 +0000

    Copy the unit tests from rt to rt2
    * Added jaxb util classes unit tests
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@436196 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt2/databinding/jaxb/src/test/java/org/objectweb/celtix/jaxb/utils/DepthXMLStreamReaderTest.java
@@ -0,0 +1,40 @@
+package org.objectweb.celtix.jaxb.utils;
+
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+
+import junit.framework.TestCase;
+
+public class DepthXMLStreamReaderTest extends TestCase {
+    public void testReader() throws Exception {
+        XMLInputFactory ifactory = StaxUtils.getXMLInputFactory();
+        XMLStreamReader reader = 
+            ifactory.createXMLStreamReader(getClass().getResourceAsStream("../resources/amazon.xml"));
+        
+        DepthXMLStreamReader dr = new DepthXMLStreamReader(reader);
+        
+        StaxUtils.toNextElement(dr);
+        assertEquals("ItemLookup", dr.getLocalName());
+        assertEquals(XMLStreamReader.START_ELEMENT, reader.getEventType());
+
+        assertEquals(1, dr.getDepth());
+
+        assertEquals(0, dr.getAttributeCount());
+
+
+        dr.next();
+
+        assertEquals(1, dr.getDepth());
+        assertTrue(dr.isWhiteSpace());
+
+        dr.nextTag();
+
+        assertEquals(2, dr.getDepth());
+        assertEquals("SubscriptionId", dr.getLocalName());
+
+        dr.next();
+        assertEquals("1E5AY4ZG53H4AMC8QH82", dr.getText());
+        
+        dr.close();
+    }
+}
---
+++ rt2/databinding/jaxb/src/test/java/org/objectweb/celtix/jaxb/utils/StaxStreamFilterTest.java
@@ -0,0 +1,65 @@
+package org.objectweb.celtix.jaxb.utils;
+
+import java.io.*;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamReader;
+
+import junit.framework.TestCase;
+import static org.objectweb.celtix.datamodel.soap.SOAPConstants.SOAP_BODY;
+import static org.objectweb.celtix.datamodel.soap.SOAPConstants.SOAP_ENV;
+
+public class StaxStreamFilterTest extends TestCase {
+
+    public void testFilter() throws Exception {
+        StaxStreamFilter filter = new StaxStreamFilter(new QName[]{SOAP_ENV, SOAP_BODY});
+        String soapMessage = "..//resources/sayHiRpcLiteralReq.xml";
+        XMLStreamReader reader = StaxUtils.createXMLStreamReader(getTestStream(soapMessage));
+        reader = StaxUtils.createFilteredReader(reader, filter);
+        
+        DepthXMLStreamReader dr = new DepthXMLStreamReader(reader);
+
+        StaxUtils.toNextElement(dr);
+        QName sayHi = new QName("http://objectweb.org/hello_world_rpclit", "sayHi");
+        
+        assertEquals(sayHi, dr.getName());
+    }
+
+    public void testFilterRPC() throws Exception {
+        StaxStreamFilter filter = new StaxStreamFilter(new QName[]{SOAP_ENV, SOAP_BODY});
+        String soapMessage = "../resources/greetMeRpcLitReq.xml";
+        XMLStreamReader reader = StaxUtils.createXMLStreamReader(getTestStream(soapMessage));
+        reader = StaxUtils.createFilteredReader(reader, filter);
+        
+        DepthXMLStreamReader dr = new DepthXMLStreamReader(reader);
+
+        StaxUtils.toNextElement(dr);
+        assertEquals(new QName("http://objectweb.org/hello_world_rpclit", "sendReceiveData"), dr.getName());
+
+        StaxUtils.nextEvent(dr);
+        StaxUtils.toNextElement(dr);
+        assertEquals(new QName("", "in"), dr.getName());
+
+        StaxUtils.nextEvent(dr);
+        StaxUtils.toNextElement(dr);
+        assertEquals(new QName("http://objectweb.org/hello_world_rpclit/types", "elem1"), dr.getName());
+
+        StaxUtils.nextEvent(dr);
+        StaxUtils.toNextText(dr);
+        assertEquals("this is element 1", dr.getText());
+        
+        StaxUtils.toNextElement(dr);
+        assertEquals(new QName("http://objectweb.org/hello_world_rpclit/types", "elem1"), dr.getName());
+        assertEquals(XMLStreamConstants.END_ELEMENT, dr.getEventType());
+
+        StaxUtils.nextEvent(dr);
+        StaxUtils.toNextElement(dr);
+        
+        assertEquals(new QName("http://objectweb.org/hello_world_rpclit/types", "elem2"), dr.getName());
+    } 
+
+    private InputStream getTestStream(String file) {
+        return getClass().getResourceAsStream(file);
+    }
+}
