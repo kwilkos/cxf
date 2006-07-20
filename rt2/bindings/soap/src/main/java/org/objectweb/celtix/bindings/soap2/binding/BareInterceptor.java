2b799fc8411da1ce4b81d3509440221f02fdf52a
James Maode Mao <mmao@apache.org>
2006-07-20 03:48:37 +0000

    Support doc-lit bare style.
    * Added inbound/outbound processing.
    * Currently based on callback, should be changed later.
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@436265 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt2/bindings/soap/src/main/java/org/objectweb/celtix/bindings/soap2/binding/BareInterceptor.java
@@ -0,0 +1,102 @@
+package org.objectweb.celtix.bindings.soap2.binding;
+
+import java.io.*;
+import java.util.*;
+
+import javax.jws.WebParam;
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamReader;
+import org.objectweb.celtix.bindings.DataBindingCallback;
+import org.objectweb.celtix.bindings.DataReader;
+import org.objectweb.celtix.bindings.soap2.SoapMessage;
+import org.objectweb.celtix.bindings.soap2.SoapVersion;
+import org.objectweb.celtix.message.Message;
+import org.objectweb.celtix.phase.AbstractPhaseInterceptor;
+import org.objectweb.celtix.staxutils.DepthXMLStreamReader;
+import org.objectweb.celtix.staxutils.StaxStreamFilter;
+import org.objectweb.celtix.staxutils.StaxUtils;
+
+public class BareInterceptor extends AbstractPhaseInterceptor {
+
+    private static final String INBOUND_MESSAGE = "message.inbound";
+    
+    private SoapMessage soapMessage;
+    private DepthXMLStreamReader xmlReader;
+
+    private void init(Message message) {
+        if (!(message instanceof SoapMessage)) {
+            return;
+        }
+        this.soapMessage = (SoapMessage) message;
+        
+        this.xmlReader = getXMLStreamReader();
+    }
+
+    public void handleMessage(Message message) {
+        init(message);
+
+        if (!isInboundMessage() && getCallback().getWebResult() != null) {
+            Object retVal = getDataReader().read(getCallback().getWebResultQName(),
+                                                 -1,
+                                                 this.xmlReader);
+            message.put("RETURN", retVal);
+        }
+
+        
+        List<Object> parameters = new ArrayList<Object>();
+        WebParam.Mode ignoreParamMode = isInboundMessage() ? WebParam.Mode.OUT : WebParam.Mode.IN;
+        int noArgs = getCallback().getParamsLength();
+
+        StaxUtils.nextEvent(this.xmlReader);
+
+        for (int idx = 0; idx < noArgs; idx++) {
+            StaxUtils.toNextElement(this.xmlReader);
+            WebParam param = getCallback().getWebParam(idx);
+            if ((param.mode() != ignoreParamMode) && !param.header()) {
+                QName elName = new QName(param.targetNamespace(), param.name());
+
+                Object obj = getDataReader().read(elName, idx, this.xmlReader);
+
+                parameters.add(obj);
+            }
+        }
+        
+        message.put("PARAMETERS", parameters);
+        
+        message.getInterceptorChain().doIntercept(message);
+    }
+
+    protected DataReader<XMLStreamReader> getDataReader() {
+        DataBindingCallback callback = getCallback();
+
+        DataReader<XMLStreamReader> dataReader = null;
+        for (Class<?> cls : callback.getSupportedFormats()) {
+            if (cls == XMLStreamReader.class) {
+                dataReader = callback.createReader(XMLStreamReader.class);
+                break;
+            }
+        }
+
+        if (dataReader == null) {
+            throw new RuntimeException("Could not figure out how to unmarshal data");
+        }
+        return dataReader;
+    }
+
+    protected boolean isInboundMessage() {
+        return this.soapMessage.containsKey(INBOUND_MESSAGE);
+    }
+
+    private DataBindingCallback getCallback() {
+        return (DataBindingCallback) this.soapMessage.get("JAXB_CALLBACK");
+    }
+
+    private DepthXMLStreamReader getXMLStreamReader() {
+        SoapVersion version = this.soapMessage.getVersion();
+        XMLStreamReader xr = StaxUtils.createXMLStreamReader(this.soapMessage.getSource(InputStream.class));
+        StaxStreamFilter filter = new StaxStreamFilter(new QName[]{version.getEnvelope(),
+                                                                   version.getBody()});
+        xr = StaxUtils.createFilteredReader(xr, filter);
+        return new DepthXMLStreamReader(xr);
+    }
+}
---
+++ rt2/bindings/soap/src/test/java/org/objectweb/celtix/bindings/soap2/binding/BareOutInterceptorTest.java
@@ -0,0 +1,93 @@
+package org.objectweb.celtix.bindings.soap2.binding;
+
+import java.io.*;
+import java.util.*;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.XMLStreamWriter;
+import javax.xml.ws.Holder;
+import javax.xml.ws.handler.MessageContext;
+import org.objectweb.celtix.bindings.soap2.TestBase;
+import org.objectweb.celtix.context.ObjectMessageContextImpl;
+import org.objectweb.celtix.staxutils.DepthXMLStreamReader;
+import org.objectweb.celtix.staxutils.StaxUtils;
+import org.objectweb.hello_world_doc_lit_bare.PutLastTradedPricePortType;
+import org.objectweb.hello_world_doc_lit_bare.types.TradePriceData;
+
+public class BareOutInterceptorTest extends TestBase {
+
+    private ByteArrayOutputStream baos;
+    private ObjectMessageContextImpl objCtx;
+    
+    public void setUp() throws Exception {
+        super.setUp();
+        
+        BareOutInterceptor interceptor = new BareOutInterceptor();
+        interceptor.setPhase("phase1");
+        chain.add(interceptor);
+
+        baos =  new ByteArrayOutputStream();
+
+        soapMessage.setResult(XMLStreamWriter.class, getXMLStreamWriter(baos));
+        soapMessage.put(MessageContext.WSDL_OPERATION, "sayHi");
+        soapMessage.put("JAXB_CALLBACK", getTestCallback(PutLastTradedPricePortType.class, "sayHi"));
+    }
+
+    public void tearDown() throws Exception {
+        baos.close();
+    }
+
+    public void testWriteOutbound() throws Exception {
+        Holder<TradePriceData> holder = new Holder<TradePriceData>(getTestObject());
+        soapMessage.put("PARAMETERS", Arrays.asList(holder));
+        soapMessage.getInterceptorChain().doIntercept(soapMessage);
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        XMLStreamReader xr = StaxUtils.createXMLStreamReader(bais);
+        DepthXMLStreamReader reader = new DepthXMLStreamReader(xr);
+        StaxUtils.toNextElement(reader);
+        assertEquals(new QName("http://objectweb.org/hello_world_doc_lit_bare/types", "inout"),
+                     reader.getName());
+        
+        StaxUtils.nextEvent(reader);
+        StaxUtils.toNextElement(reader);
+        assertEquals(new QName("http://objectweb.org/hello_world_doc_lit_bare/types", "tickerSymbol"),
+                     reader.getName());
+
+        StaxUtils.nextEvent(reader);
+        StaxUtils.toNextText(reader);
+        assertEquals("this is elem1", reader.getText());
+    }
+
+    public void testWriteInbound() throws Exception {
+        Holder<TradePriceData> holder = new Holder<TradePriceData>(getTestObject());
+        soapMessage.put("PARAMETERS", Arrays.asList(holder));
+        soapMessage.put("message.inbound", "message.inbound");
+        
+        soapMessage.getInterceptorChain().doIntercept(soapMessage);
+
+        ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+        XMLStreamReader xr = StaxUtils.createXMLStreamReader(bais);
+        DepthXMLStreamReader reader = new DepthXMLStreamReader(xr);
+        StaxUtils.toNextElement(reader);
+        assertEquals(new QName("http://objectweb.org/hello_world_doc_lit_bare/types", "inout"),
+                     reader.getName());
+        
+        StaxUtils.nextEvent(reader);
+        StaxUtils.toNextElement(reader);
+        assertEquals(new QName("http://objectweb.org/hello_world_doc_lit_bare/types", "tickerSymbol"),
+                     reader.getName());
+
+        StaxUtils.nextEvent(reader);
+        StaxUtils.toNextText(reader);
+        assertEquals("this is elem1", reader.getText());
+    }
+
+    public TradePriceData getTestObject() {
+        TradePriceData obj = new TradePriceData();
+        obj.setTickerSymbol("this is elem1");
+        obj.setTickerPrice(5);
+        return obj;
+    }    
+}
