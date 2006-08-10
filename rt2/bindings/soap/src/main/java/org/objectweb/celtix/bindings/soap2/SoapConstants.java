427ef40be7ceae1a3adfd83f5a2546844f58cf33
Tom Li <tli@apache.org>
2006-08-10 12:27:06 +0000

    CXF-48 added missing SoapConstants class
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@436398 13f79535-47bb-0310-9956-ffa450edef68

---
+++ rt2/bindings/soap/src/main/java/org/objectweb/celtix/bindings/soap2/SoapConstants.java
@@ -0,0 +1,7 @@
+package org.objectweb.celtix.bindings.soap2;
+
+public class SoapConstants {
+    public static final String STYLE_RPC = "rpc";
+    public static final String STYLE_BARE = "bare";
+    public static final String STYLE_WRAPPED = "wrapped";
+}
