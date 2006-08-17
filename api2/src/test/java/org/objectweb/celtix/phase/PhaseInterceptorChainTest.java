9dc1d12b0de92eaa3852d22b38d949df5486ef7d
Andrea Smyth <andreasmyth@apache.org>
2006-08-17 17:49:07 +0000

    Added tests for (static and dynamic) addition of multiple interceptors for same phase.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@436449 13f79535-47bb-0310-9956-ffa450edef68

--- api2/src/test/java/org/objectweb/celtix/phase/PhaseInterceptorChainTest.java
+++ api2/src/test/java/org/objectweb/celtix/phase/PhaseInterceptorChainTest.java
@@ -49,11 +49,11 @@ public class PhaseInterceptorChainTest extends TestCase {
     }
     
     @SuppressWarnings("unchecked")
-    public void xtestAddTwoInterceptorsSamePhase() {
+    public void testAddTwoInterceptorsSamePhase() {
         AbstractPhaseInterceptor p1 = setUpPhaseInterceptor("phase1", "p1");
-        Set<String> before = new HashSet<String>();
-        before.add("p1");
-        AbstractPhaseInterceptor p2 = setUpPhaseInterceptor("phase1", "p2", before);
+        Set<String> after = new HashSet<String>();
+        after.add("p1");
+        AbstractPhaseInterceptor p2 = setUpPhaseInterceptor("phase1", "p2", after);
         control.replay();
         chain.add(p1);
         chain.add(p2);
@@ -154,6 +154,24 @@ public class PhaseInterceptorChainTest extends TestCase {
         assertEquals(0, p1.faultInvoked);
     }
     
+    public void testInsertionInSamePhasePass() {
+        
+        AbstractPhaseInterceptor p2 = setUpPhaseInterceptor("phase1", "p2");
+        setUpPhaseInterceptorInvocations(p2, false, false);
+        Set<String> after3 = new HashSet<String>();
+        after3.add("p2");
+        AbstractPhaseInterceptor p3 = setUpPhaseInterceptor("phase1", "p3", after3);
+        setUpPhaseInterceptorInvocations(p3, false, false);
+        InsertingPhaseInterceptor p1 = new InsertingPhaseInterceptor(chain, p3, "phase1", "p1");
+        p1.addBefore("p2");
+        control.replay();   
+        chain.add(p1);
+        chain.add(p2);
+        chain.doIntercept(message);
+        assertEquals(1, p1.invoked);
+        assertEquals(0, p1.faultInvoked);
+    }
+    
     AbstractPhaseInterceptor setUpPhaseInterceptor(String phase, 
                                                    String id) {
         return setUpPhaseInterceptor(phase, id, null);     
@@ -162,12 +180,12 @@ public class PhaseInterceptorChainTest extends TestCase {
     @SuppressWarnings("unchecked")
     AbstractPhaseInterceptor setUpPhaseInterceptor(String phase, 
                                                    String id,
-                                                   Set<String> b) {
+                                                   Set<String> a) {
         AbstractPhaseInterceptor p = control.createMock(AbstractPhaseInterceptor.class);
         EasyMock.expect(p.getPhase()).andReturn(phase).anyTimes();
         EasyMock.expect(p.getId()).andReturn(id).anyTimes();
-        Set<String> before = null == b ? new HashSet<String>() : b;
-        Set<String> after = new HashSet<String>();
+        Set<String> before = new HashSet<String>();
+        Set<String> after = null == a ? new HashSet<String>() : a;
         EasyMock.expect(p.getBefore()).andReturn(before).anyTimes();
         EasyMock.expect(p.getAfter()).andReturn(after).anyTimes();
         
@@ -191,104 +209,6 @@ public class PhaseInterceptorChainTest extends TestCase {
             EasyMock.expectLastCall();
         } 
     }
-
-    /*
-    public void testPhaseResolution() {
- 
-        Phase1Interceptor i1 = new Phase1Interceptor();
-        i1.setId("i1");
-        Phase2Interceptor i2 = new Phase2Interceptor();
-        chain.add(i1);
-        chain.add(i2);
-        
-        MessageImpl message = new MessageImpl();
-        message.setInterceptorChain(chain);
-        chain.doIntercept(message);
-        
-        assertEquals(1, i1.invoked);
-        assertEquals(1, i2.invoked);
-    }
-
-    public void dontTestReversePhaseResolution() {
-        Phase phase1 = new Phase("phase1", 2);
-        Phase phase2 = new Phase("phase2", 1);
-        
-        List<Phase> phases = new ArrayList<Phase>();
-        phases.add(phase1);
-        phases.add(phase2);
-        
-        PhaseInterceptorChain chain = new PhaseInterceptorChain(phases);
-        
-        Phase1Interceptor i1 = new Phase1Interceptor();
-        Phase2Interceptor i2 = new Phase2Interceptor();
-        chain.add(i1);
-        chain.add(i2);
-        
-        MessageImpl message = new MessageImpl();
-        message.setInterceptorChain(chain);
-        chain.doIntercept(message);
-        
-        assertEquals(0, i1.invoked);
-        assertEquals(1, i2.invoked);
-    }
-    
-    public void dontTestInterphaseResolution() {
-        Phase phase1 = new Phase("phase1", 1);
-        Phase phase2 = new Phase("phase2", 2);
-        
-        List<Phase> phases = new ArrayList<Phase>();
-        phases.add(phase1);
-        phases.add(phase2);
-        
-        PhaseInterceptorChain chain = new PhaseInterceptorChain(phases);
-        
-        Phase1Interceptor i1 = new Phase1Interceptor();
-        i1.setId("i1");
-        i1.addAfter("i2");
-        Phase2Interceptor i2 = new Phase2Interceptor();
-        i2.setPhase("phase1");
-        i2.setId("i2");
-        i2.addBefore("i1");
-        
-        chain.add(i1);
-        chain.add(i2);
-        
-        MessageImpl message = new MessageImpl();
-        message.setInterceptorChain(chain);
-        chain.doIntercept(message);
-        
-        assertEquals(0, i1.invoked);
-        assertEquals(1, i2.invoked);
-        
-        chain.doIntercept(message);
-        assertEquals(1, i1.invoked);
-        assertEquals(1, i2.invoked);
-    }
-    
-    public class Phase1Interceptor extends AbstractPhaseInterceptor<Message> {
-        int invoked;
-        
-        public Phase1Interceptor() {
-            setPhase("phase1");
-        }
-
-        public void handleMessage(Message message) {
-            invoked++;
-            // message.getInterceptorChain().doIntercept(message);
-        }
-    }
-    
-    public class Phase2Interceptor extends AbstractPhaseInterceptor<Message> {
-        int invoked;
-        public Phase2Interceptor() {
-            setPhase("phase2");
-        }
-
-        public void handleMessage(Message message) {
-            invoked++;
-        }
-    }
-    */
     
     public class InsertingPhaseInterceptor extends AbstractPhaseInterceptor<Message> {
         int invoked;
@@ -302,7 +222,7 @@ public class PhaseInterceptorChainTest extends TestCase {
                                          String phase, 
                                          String id) {
             setPhase(phase);
-            setId(id);
+            setId(id);            
             insertionChain = c;
             insertionInterceptor = i;
         }
