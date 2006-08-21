bb8e8af75d89da5cde4f3957ad722f09c0e40a1d
Andrea Smyth <andreasmyth@apache.org>
2006-08-21 13:37:26 +0000

    Overwrite setServiceClass in JaxWsServiceFactoryBean to also update the wsdl location. Obtain the latter from wsdlLocation attribute in WebService annotation of either implementor or SEI - not using EndpointReferenceUtils.getWebServiceAnnotation because a) algorithm not correct/usable for that purpose and b) we should start moving jaxws specific things into frontend, possibly also into a utility class there. Allows EndpointTest to pass (and setWsdlURL in GreeterTest is not necessary).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@436473 13f79535-47bb-0310-9956-ffa450edef68

--- rt2/frontend/jaxws/src/main/java/org/objectweb/celtix/jaxws/support/JaxWsServiceFactoryBean.java
+++ rt2/frontend/jaxws/src/main/java/org/objectweb/celtix/jaxws/support/JaxWsServiceFactoryBean.java
@@ -2,10 +2,20 @@ package org.objectweb.celtix.jaxws.support;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+import javax.jws.WebService;
 import javax.wsdl.WSDLException;
+import javax.xml.ws.WebServiceException;
 
 import org.objectweb.celtix.BusException;
+import org.objectweb.celtix.common.classloader.ClassLoaderUtils;
+import org.objectweb.celtix.common.i18n.Message;
+import org.objectweb.celtix.common.logging.LogUtils;
 import org.objectweb.celtix.endpoint.ServerImpl;
 import org.objectweb.celtix.interceptors.WrappedInInterceptor;
 import org.objectweb.celtix.interceptors.WrappedOutInterceptor;
@@ -20,6 +30,9 @@ import org.objectweb.celtix.service.model.OperationInfo;
 
 public class JaxWsServiceFactoryBean extends ReflectionServiceFactoryBean {
 
+    private static final Logger LOG = LogUtils.getL7dLogger(JaxWsServiceFactoryBean.class);
+    private static final ResourceBundle BUNDLE = LOG.getResourceBundle();
+    
     public JaxWsServiceFactoryBean() {
         super();
 
@@ -57,5 +70,48 @@ public class JaxWsServiceFactoryBean extends ReflectionServiceFactoryBean {
             o.setProperty(WrappedOutInterceptor.SINGLE_WRAPPED_PART, responseWrapper);
         }
     }
+
+    @Override
+    public void setServiceClass(Class<?> serviceClass) {
+        super.setServiceClass(serviceClass);
+        
+        // update wsdl location
+        
+        // TODO: replace version in EndpointreferenceUtils?
+        
+        WebService ws = serviceClass.getAnnotation(WebService.class);
+        if (null == ws) {
+            // endpoint must be a provider type endpoint 
+            // TODO: get wsdl location from WwebServiceProvider annotation
+            // return;
+        }
+        
+        String sei = ws.endpointInterface();
+        if (null != sei && !"".equals(sei)) {
+            Class<?> seiClass = null;
+            try {
+                seiClass = ClassLoaderUtils.loadClass(sei, serviceClass);
+            } catch (ClassNotFoundException ex) {
+                throw new WebServiceException(BUNDLE.getString("SEI_LOAD_FAILURE_MSG"), ex);
+            }
+            ws = seiClass.getAnnotation(WebService.class);
+            if (null == ws) {
+                throw new WebServiceException(BUNDLE.getString("SEI_WITHOUT_WEBSERVICE_ANNOTATION_EXC"));
+            }
+        }
+        if (null != ws.wsdlLocation() && !"".equals(ws.wsdlLocation())) {
+            
+            if (LOG.isLoggable(Level.FINE)) {
+                LOG.fine("Setting wsdl location to:  " + ws.wsdlLocation());
+            }
+            try {
+                setWsdlURL(new URL(ws.wsdlLocation()));
+            } catch (MalformedURLException ex) {
+                Message msg = new Message("MALFORMED_URL_IN_WEBSERVICE_ANNOTATION_EXC", 
+                                          BUNDLE, ws.wsdlLocation());
+                throw new WebServiceException(msg.toString(), ex);                
+            }
+        }
+    }  
     
 }
