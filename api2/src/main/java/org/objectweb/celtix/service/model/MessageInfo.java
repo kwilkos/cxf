02d29cf30259cb0f61352010e82df37f64ca3719
Andrea Smyth <andreasmyth@apache.org>
2006-08-22 14:16:16 +0000

    Mocked bus and required service model elements in OutgoingChainInterceptorTest.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@436488 13f79535-47bb-0310-9956-ffa450edef68

--- api2/src/main/java/org/objectweb/celtix/service/model/MessageInfo.java
+++ api2/src/main/java/org/objectweb/celtix/service/model/MessageInfo.java
@@ -3,7 +3,7 @@ package org.objectweb.celtix.service.model;
 
 import javax.xml.namespace.QName;
 
-public final class MessageInfo extends AbstractMessageContainer {
+public class MessageInfo extends AbstractMessageContainer {
     public MessageInfo(OperationInfo op, QName nm) {
         super(op, nm);
     }
--- rt2/core/src/test/java/org/objectweb/celtix/interceptors/OutgoingChainInterceptorTest.java
+++ rt2/core/src/test/java/org/objectweb/celtix/interceptors/OutgoingChainInterceptorTest.java
@@ -3,13 +3,13 @@ package org.objectweb.celtix.interceptors;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.xml.namespace.QName;
+// import javax.xml.namespace.QName;
 
 import junit.framework.TestCase;
 
 import org.easymock.IMocksControl;
+import org.easymock.classextension.EasyMock;
 import org.objectweb.celtix.Bus;
-import org.objectweb.celtix.bus.CeltixBusFactory;
 import org.objectweb.celtix.endpoint.Endpoint;
 import org.objectweb.celtix.message.Exchange;
 import org.objectweb.celtix.message.ExchangeImpl;
@@ -17,61 +17,72 @@ import org.objectweb.celtix.message.MessageImpl;
 import org.objectweb.celtix.phase.Phase;
 import org.objectweb.celtix.phase.PhaseManager;
 import org.objectweb.celtix.service.Service;
+import org.objectweb.celtix.service.model.BindingMessageInfo;
 import org.objectweb.celtix.service.model.BindingOperationInfo;
-import org.objectweb.celtix.service.model.EndpointInfo;
-import org.objectweb.celtix.wsdl11.WSDLServiceFactory;
+import org.objectweb.celtix.service.model.MessageInfo;
+import org.objectweb.celtix.service.model.OperationInfo;
 
-import static org.easymock.EasyMock.expect;
-import static org.easymock.classextension.EasyMock.createNiceControl;
 
 public class OutgoingChainInterceptorTest extends TestCase {
-    
-    Endpoint endpoint;
-    Bus bus;
-    String ns = "http://objectweb.org/hello_world_soap_http";
-    private BindingOperationInfo op;
-    
-    
-    @Override
+
+    private IMocksControl control;
+    private Bus bus;
+    private Service service;
+    private Endpoint endpoint;
+    private BindingOperationInfo bopInfo;
+    private OperationInfo opInfo;
+    private BindingMessageInfo bmInfo;
+    private MessageInfo mInfo;
+    private List<Phase> phases;
+    private List<Interceptor> empty;
+
     protected void setUp() throws Exception {
         super.setUp();
-        
-        IMocksControl control = createNiceControl();
-        bus = new CeltixBusFactory().createBus();
-        PhaseManager pm = control.createMock(PhaseManager.class);
-        
-        List<Phase> phases = new ArrayList<Phase>();
+
+        control = EasyMock.createNiceControl();
+
+        phases = new ArrayList<Phase>();
         phases.add(new Phase(Phase.SEND, 1000));
-        bus.setExtension(pm, PhaseManager.class);
-        expect(pm.getOutPhases()).andReturn(phases);
+        empty = new ArrayList<Interceptor>();
 
+        bus = control.createMock(Bus.class);
+        PhaseManager pm = control.createMock(PhaseManager.class);
+        EasyMock.expect(bus.getExtension(PhaseManager.class)).andReturn(pm);
+        EasyMock.expect(pm.getOutPhases()).andReturn(phases);
+
+        service = control.createMock(Service.class);
         endpoint = control.createMock(Endpoint.class);
+        EasyMock.expect(endpoint.getService()).andReturn(service);
+        EasyMock.expect(endpoint.getOutInterceptors()).andReturn(empty);
+        EasyMock.expect(service.getOutInterceptors()).andReturn(empty);
 
-        WSDLServiceFactory factory = new WSDLServiceFactory(bus, 
-            getClass().getResource("/wsdl/hello_world.wsdl"),
-            new QName(ns, "SOAPService"));
-            
-        Service service = factory.create();
-        
-        EndpointInfo endpointInfo = service.getServiceInfo().getEndpoint(new QName(ns, "SoapPort"));
-        op = endpointInfo.getBinding().getOperation(new QName(ns, "greetMe"));
-        
-        expect(endpoint.getService()).andReturn(service).anyTimes();
+        bopInfo = control.createMock(BindingOperationInfo.class);
+        opInfo = control.createMock(OperationInfo.class);
+        mInfo = control.createMock(MessageInfo.class);
+        bmInfo = control.createMock(BindingMessageInfo.class);
+        EasyMock.expect(bopInfo.getOperationInfo()).andReturn(opInfo).times(2);
+        EasyMock.expect(opInfo.getOutput()).andReturn(mInfo);
+        EasyMock.expect(bopInfo.getOutput()).andReturn(bmInfo);
 
         control.replay();
+
+    }
+
+    public void tearDown() {
+        control.verify();
     }
 
     public void testInterceptor() throws Exception {
         OutgoingChainSetupInterceptor setupIntc = new OutgoingChainSetupInterceptor();
         OutgoingChainInterceptor intc = new OutgoingChainInterceptor();
-        
+
         MessageImpl m = new MessageImpl();
         Exchange exchange = new ExchangeImpl();
         m.setExchange(exchange);
         exchange.setOutMessage(m);
         exchange.put(Bus.class, bus);
         exchange.put(Endpoint.class, endpoint);
-        exchange.put(BindingOperationInfo.class, op);
+        exchange.put(BindingOperationInfo.class, bopInfo);
         setupIntc.handleMessage(m);
         intc.handleMessage(m);
     }
