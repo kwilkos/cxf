f0480f9ab66f5b5920bcd0f8cecb0c529e3d9f2a
Andrea Smyth <andreasmyth@apache.org>
2006-09-06 16:54:29 +0000

    Fixed some checkstyle errors.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@440793 13f79535-47bb-0310-9956-ffa450edef68

--- tools/xjc/cfg/src/main/java/org/apache/cxf/xjc/cfg/ConfigurableBeansPlugin.java
+++ tools/xjc/cfg/src/main/java/org/apache/cxf/xjc/cfg/ConfigurableBeansPlugin.java
@@ -1,13 +1,29 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.cxf.xjc.cfg;
 
-import java.awt.Image;
 import java.util.Collections;
 import java.util.List;
 
 import org.xml.sax.ErrorHandler;
 
-import com.sun.codemodel.JBlock;
-import com.sun.codemodel.JCodeModel;
 import com.sun.codemodel.JConditional;
 import com.sun.codemodel.JDefinedClass;
 import com.sun.codemodel.JDocComment;
@@ -20,16 +36,10 @@ import com.sun.codemodel.JOp;
 import com.sun.codemodel.JType;
 import com.sun.tools.xjc.Options;
 import com.sun.tools.xjc.Plugin;
-import com.sun.tools.xjc.model.CCustomizations;
 import com.sun.tools.xjc.model.CPluginCustomization;
-import com.sun.tools.xjc.model.CPropertyInfo;
 import com.sun.tools.xjc.outline.ClassOutline;
 import com.sun.tools.xjc.outline.FieldOutline;
 import com.sun.tools.xjc.outline.Outline;
-import com.sun.tools.xjc.util.DOMUtils;
-import com.sun.xml.xsom.XSElementDecl;
-import com.sun.xml.xsom.XSParticle;
-import com.sun.xml.xsom.XSTerm;
 
 import org.apache.cxf.configuration.AbstractConfigurableBeanBase;
 
--- tools/xjc/dv/src/main/java/org/apache/cxf/xjc/dv/DefaultValuePlugin.java
+++ tools/xjc/dv/src/main/java/org/apache/cxf/xjc/dv/DefaultValuePlugin.java
@@ -1,3 +1,22 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.cxf.xjc.dv;
 
 import java.util.Map;
@@ -117,11 +136,11 @@ public class DefaultValuePlugin extends Plugin {
                         xsType = xsType.getBaseType();
                     }
                     if ("base64Binary".equals(xsType.getName())) {
-                       // var.init(outline.getCodeModel().ref(DatatypeConverter.class)
-                       //          .staticInvoke("parseBase64Binary").arg(defaultValue));
+                        var.init(outline.getCodeModel().ref(DatatypeConverter.class)
+                           .staticInvoke("parseBase64Binary").arg(defaultValue));
                     } else if ("hexBinary".equals(xsType.getName())) {
-                        // var.init(JExpr._new(outline.getCodeModel().ref(HexBinaryAdapter.class))
-                        //    .invoke("unmarshal").arg(defaultValue));
+                        var.init(JExpr._new(outline.getCodeModel().ref(HexBinaryAdapter.class))
+                            .invoke("unmarshal").arg(defaultValue));
                     }
                 } else if ("javax.xml.namespace.QName".equals(typeName)) {
                     NamespaceContext nsc = new NamespaceContextAdapter(xmlDefaultValue);
