f9e825a798470ca9062a6b68f86be9a7167cbd85
Jervis Liu <jliu@apache.org>
2006-10-26 03:59:09 +0000

    Enable two ws_a system tests that were disabled by my previous commit.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@467855 13f79535-47bb-0310-9956-ffa450edef68

--- systests/src/test/java/org/apache/cxf/systest/ws/addressing/MAPTest.java
+++ systests/src/test/java/org/apache/cxf/systest/ws/addressing/MAPTest.java
@@ -132,7 +132,7 @@ public class MAPTest extends ClientServerTestBase implements VerificationCache {
 
     //--Tests
      
-    public void xtestImplicitMAPs() throws Exception {
+    public void testImplicitMAPs() throws Exception {
         try {
             String greeting = greeter.greetMe("implicit1");
             assertEquals("unexpected response received from service", 
@@ -247,7 +247,7 @@ public class MAPTest extends ClientServerTestBase implements VerificationCache {
         }
     }
 
-    public void xtestVersioning() throws Exception {
+    public void testVersioning() throws Exception {
         try {
             // expect two MAPs instances versioned with 200408, i.e. for both 
             // the partial and full responses
