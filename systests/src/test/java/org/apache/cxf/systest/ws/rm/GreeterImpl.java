a93572aa11eaac17336c0925462b02395ffc0015
Andrea Smyth <andreasmyth@apache.org>
2007-03-05 13:27:30 +0000

    Refactored some system test code. Removed unused wsdls and ClientServerGreeterTest (which did not test anything that is not already covered by other system tests in jaxws and handlers package).
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@514664 13f79535-47bb-0310-9956-ffa450edef68

--- systests/src/test/java/org/apache/cxf/systest/ws/rm/GreeterImpl.java
+++ systests/src/test/java/org/apache/cxf/systest/ws/rm/GreeterImpl.java
@@ -19,19 +19,9 @@
 
 package org.apache.cxf.systest.ws.rm;
 
-import java.util.concurrent.Future;
-import java.util.logging.Logger;
-
 import javax.jws.WebService;
-import javax.xml.ws.AsyncHandler;
-import javax.xml.ws.Response;
 
-import org.apache.cxf.greeter_control.Greeter;
-import org.apache.cxf.greeter_control.PingMeFault;
-import org.apache.cxf.greeter_control.types.FaultDetail;
-import org.apache.cxf.greeter_control.types.GreetMeResponse;
-import org.apache.cxf.greeter_control.types.PingMeResponse;
-import org.apache.cxf.greeter_control.types.SayHiResponse;
+import org.apache.cxf.greeter_control.AbstractGreeterImpl;
 
 /**
  * 
@@ -41,91 +31,5 @@ import org.apache.cxf.greeter_control.types.SayHiResponse;
             portName = "GreeterPort",
             endpointInterface = "org.apache.cxf.greeter_control.Greeter",
             targetNamespace = "http://cxf.apache.org/greeter_control")
-public class GreeterImpl implements Greeter {
-
-    private static final Logger LOG = Logger.getLogger(GreeterImpl.class.getName());
-    private long delay;
-    private String lastOnewayArg;
-    private int pingMeCount;
-     
-    public long getDelay() {
-        return delay;
-    }
-
-    public void setDelay(long d) {
-        delay = d;
-    }
-
-    public String greetMe(String arg0) {
-        LOG.fine("Executing operation greetMe with parameter: " + arg0);        
-        if (delay > 0) {
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException ex) {
-                // ignore
-            }
-        }
-        String result = null;
-        synchronized (this) {
-            result = null == lastOnewayArg ? arg0.toUpperCase() : lastOnewayArg;
-        }
-        LOG.fine("returning: " + result);
-        return result;
-    }
-
-    public Future<?> greetMeAsync(String arg0, AsyncHandler<GreetMeResponse> arg1) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public Response<GreetMeResponse> greetMeAsync(String arg0) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public void greetMeOneWay(String arg0) {
-        synchronized (this) {
-            lastOnewayArg = arg0;
-        }
-        LOG.fine("Executing operation greetMeOneWay with parameter: " + arg0);
-    }
-
-    public void pingMe() throws PingMeFault {
-        pingMeCount++;
-        if ((pingMeCount % 2) > 0) {
-            LOG.fine("Executing operation pingMe");        
-        } else {
-            LOG.fine("Throwing PingMeFault while executiong operation pingMe");
-            FaultDetail fd = new FaultDetail();
-            fd.setMajor((short)2);
-            fd.setMinor((short)1);
-            throw new PingMeFault("Pings succeed only every other time.", fd);
-        }
-    }
-
-    public Response<PingMeResponse> pingMeAsync() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public Future<?> pingMeAsync(AsyncHandler<PingMeResponse> arg0) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public String sayHi() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public Response<SayHiResponse> sayHiAsync() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    public Future<?> sayHiAsync(AsyncHandler<SayHiResponse> arg0) {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
+public class GreeterImpl extends AbstractGreeterImpl {
 }
