f6ae54a2ee2776098341bb9217724070487499ed
Daniel Diephouse <dandiep@apache.org>
2007-03-16 00:36:18 +0000

    Attempt to fix formatting of samples. (Where is checkstyle being run from that CC notifies me of sample formatting errors?)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@518815 13f79535-47bb-0310-9956-ffa450edef68

--- distribution/src/main/release/samples/spring_http/src/main/java/demo/spring/client/Client.java
+++ distribution/src/main/release/samples/spring_http/src/main/java/demo/spring/client/Client.java
@@ -25,19 +25,19 @@ import demo.spring.HelloWorld;
 
 public final class Client {
 
-    private Client() {
-    } 
+	private Client() {
+	}
 
-    public static void main(String args[]) throws Exception {
-    	// START SNIPPET: client
-        ClassPathXmlApplicationContext context = 
-        	new ClassPathXmlApplicationContext(new String[] {"demo/spring/client/client-beans.xml"});
-        
-        HelloWorld client = (HelloWorld) context.getBean("client");
-        
-        String response = client.sayHi("Joe");
-        System.out.println("Response: " + response);
-        System.exit(0);
-        // END SNIPPET: client
-    }
+	public static void main(String args[]) throws Exception {
+		// START SNIPPET: client
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				new String[] { "demo/spring/client/client-beans.xml" });
+
+		HelloWorld client = (HelloWorld) context.getBean("client");
+
+		String response = client.sayHi("Joe");
+		System.out.println("Response: " + response);
+		System.exit(0);
+		// END SNIPPET: client
+	}
 }
