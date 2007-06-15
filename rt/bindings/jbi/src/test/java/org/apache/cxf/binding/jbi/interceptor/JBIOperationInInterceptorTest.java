0a048a5b2c4c94e86264da6d18aa649351ce7fb9
Freeman Yue Fang <ffang@apache.org>
2007-06-15 06:39:13 +0000

    enable ignored test for JBI binding
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@547549 13f79535-47bb-0310-9956-ffa450edef68

--- rt/bindings/jbi/src/test/java/org/apache/cxf/binding/jbi/interceptor/JBIOperationInInterceptorTest.java
+++ rt/bindings/jbi/src/test/java/org/apache/cxf/binding/jbi/interceptor/JBIOperationInInterceptorTest.java
@@ -19,41 +19,61 @@
 package org.apache.cxf.binding.jbi.interceptor;
 
 
+import java.util.ResourceBundle;
+import java.util.logging.Logger;
+
 import javax.jbi.messaging.MessageExchange;
 import javax.xml.namespace.QName;
 
+import org.apache.cxf.binding.jbi.JBIBindingInfo;
+import org.apache.cxf.binding.jbi.JBIConstants;
 import org.apache.cxf.binding.jbi.JBIMessage;
+import org.apache.cxf.common.i18n.Message;
+import org.apache.cxf.common.logging.LogUtils;
 import org.apache.cxf.endpoint.Endpoint;
+import org.apache.cxf.endpoint.EndpointImpl;
+import org.apache.cxf.interceptor.Fault;
 import org.apache.cxf.message.ExchangeImpl;
 import org.apache.cxf.message.MessageImpl;
 import org.apache.cxf.phase.Phase;
 import org.apache.cxf.phase.PhaseInterceptor;
+import org.apache.cxf.service.model.EndpointInfo;
 import org.easymock.classextension.EasyMock;
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class JBIOperationInInterceptorTest extends Assert {
 
+    private static final Logger LOG = LogUtils.getL7dLogger(JBIOperationInInterceptor.class);
+    private static final ResourceBundle BUNDLE = LOG.getResourceBundle();
+    
     @Test
     public void testPhase() throws Exception {
         PhaseInterceptor<JBIMessage> interceptor = new JBIOperationInInterceptor();
         assertEquals(Phase.PRE_PROTOCOL, interceptor.getPhase());
     }
     
-    @Ignore
-    public void testOperation() throws Exception {
+    @Test
+    public void testUnknownOperation() throws Exception {
         PhaseInterceptor<JBIMessage> interceptor = new JBIOperationInInterceptor();
         JBIMessage msg = new JBIMessage(new MessageImpl());
         MessageExchange me = EasyMock.createMock(MessageExchange.class);
-        EasyMock.expect(me.getOperation()).andReturn(new QName("urn:test", "SayHi"));
+        EasyMock.expect(me.getOperation()).andReturn(new QName("urn:test", "SayHi")).times(4);
         EasyMock.replay(me);
         msg.put(MessageExchange.class, me);
-        Endpoint ep = null;
-        msg.getExchange().put(Endpoint.class, ep);
+        EndpointInfo endpointInfo = new EndpointInfo();
+        endpointInfo.setBinding(new JBIBindingInfo(null, JBIConstants.NS_JBI_BINDING));
+        Endpoint ep = new EndpointImpl(null, null, endpointInfo);
         msg.setExchange(new ExchangeImpl());
-        
-        interceptor.handleMessage(msg);
+        msg.getExchange().put(Endpoint.class, ep);
+        try { 
+            interceptor.handleMessage(msg);
+            fail("shouldn't found SayHi operation");
+        } catch (Fault fault) {
+            LOG.info(fault.getMessage());
+            assertEquals(fault.getMessage(), new Message("UNKNOWN_OPERATION", BUNDLE, 
+                                                 msg.getJbiExchange().getOperation().toString()).toString());
+        }
     }
     
     
