78d1f9fcd12d9d81bc89e3aa4fc0d27e3ffa1a0d
J. Daniel Kulp <dkulp@apache.org>
2007-06-18 18:28:25 +0000

    [CXF-597] Only add the addressing types to the JAXB context if something uses our WS-Addressing types.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@548435 13f79535-47bb-0310-9956-ffa450edef68

--- rt/databinding/jaxb/src/main/java/org/apache/cxf/jaxb/JAXBDataBinding.java
+++ rt/databinding/jaxb/src/main/java/org/apache/cxf/jaxb/JAXBDataBinding.java
@@ -71,6 +71,8 @@ import org.apache.cxf.service.Service;
 import org.apache.cxf.service.factory.ServiceConstructionException;
 import org.apache.cxf.service.model.SchemaInfo;
 import org.apache.cxf.service.model.ServiceInfo;
+import org.apache.cxf.ws.addressing.EndpointReferenceType;
+import org.apache.cxf.ws.addressing.ObjectFactory;
 import org.apache.cxf.wsdl11.WSDLServiceBuilder;
 import org.apache.ws.commons.schema.XmlSchema;
 import org.apache.ws.commons.schema.XmlSchemaCollection;
@@ -374,32 +376,32 @@ public final class JAXBDataBinding implements DataBinding {
                 classes.remove(remoteExceptionObject);
             }
         }
-
+        // add user extra class into jaxb context
+        if (extraClass != null && extraClass.length > 0) {
+            for (Class clz : extraClass) {
+                classes.add(clz);
+            }
+        }
+        
         for (Class<?> clz : classes) {
             if (clz.getName().endsWith("ObjectFactory")) {
                 //kind of a hack, but ObjectFactories may be created with empty namespaces
                 defaultNs = null;
             }
         }
-        
-        try {
-            classes.add(Class.forName("org.apache.cxf.ws.addressing.wsdl.AttributedQNameType"));
-            classes.add(Class.forName("org.apache.cxf.ws.addressing.wsdl.ObjectFactory"));
-            classes.add(Class.forName("org.apache.cxf.ws.addressing.wsdl.ServiceNameType"));
-        } catch (ClassNotFoundException e) {
-            // REVISIT - ignorable if WS-ADDRESSING not available?
-            // maybe add a way to allow interceptors to add stuff to the
-            // context?
-        }
-        try {
-            // add user extra class into jaxb context
-            if (extraClass != null && extraClass.length > 0) {
-                for (Class clz : extraClass) {
-                    classes.add(clz);
-                }
+
+        if (classes.contains(EndpointReferenceType.class)
+            || classes.contains(ObjectFactory.class)) {
+            //ws-addressing is used, lets add the specific types
+            try {
+                classes.add(Class.forName("org.apache.cxf.ws.addressing.wsdl.AttributedQNameType"));
+                classes.add(Class.forName("org.apache.cxf.ws.addressing.wsdl.ObjectFactory"));
+                classes.add(Class.forName("org.apache.cxf.ws.addressing.wsdl.ServiceNameType"));
+            } catch (ClassNotFoundException e) {
+                // REVISIT - ignorable if WS-ADDRESSING not available?
+                // maybe add a way to allow interceptors to add stuff to the
+                // context?
             }
-        } catch (Exception e) {
-           //
         }
         
         Map<String, Object> map = new HashMap<String, Object>();
