2d7079f50145214c4f7e5c225eab415836e9f383
J. Daniel Kulp <dkulp@apache.org>
2007-10-08 17:55:14 +0000

    Merged revisions 580473 via svnmerge from https://svn.apache.org/repos/asf/incubator/cxf/trunk
    ........
      r580473 | dkulp | 2007-09-28 15:57:28 -0400 (Fri, 28 Sep 2007) | 2 lines
      
      Remove stack traces when doing -Psetup.eclipse and such
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/branches/2.0.x-fixes@582915 13f79535-47bb-0310-9956-ffa450edef68

--- common/common/src/main/java/org/apache/cxf/common/util/ReflectionUtil.java
+++ common/common/src/main/java/org/apache/cxf/common/util/ReflectionUtil.java
@@ -31,18 +31,45 @@ public final class ReflectionUtil {
 
     public static List<String> getPackagesFromJar(File jarFile) throws IOException {
         List<String> packageNames = new ArrayList<String>();
-        JarResource resource = new JarResource();
-        for (String item : resource.getJarContents(jarFile)) {
-            if (!item.endsWith(".class")) {
-                continue;
-            }
-            String packageName = getPackageName(item);
-            if (!StringUtils.isEmpty(packageName)
-                && !packageNames.contains(packageName)) {
-                packageNames.add(packageName);
+        if (jarFile.isDirectory()) {
+            getPackageNamesFromDir(jarFile, jarFile, packageNames);
+        } else {
+            JarResource resource = new JarResource();
+            for (String item : resource.getJarContents(jarFile)) {
+                if (!item.endsWith(".class")) {
+                    continue;
+                }
+                String packageName = getPackageName(item);
+                if (!StringUtils.isEmpty(packageName)
+                    && !packageNames.contains(packageName)) {
+                    packageNames.add(packageName);
+                }
             }
         }
         return packageNames;
+    }
+    
+    private static void getPackageNamesFromDir(File base, File dir, List<String> pkgs) {
+        boolean foundClass = false;
+        for (File file : dir.listFiles()) {
+            if (file.isDirectory()) {
+                getPackageNamesFromDir(base, file, pkgs);
+            } else if (!foundClass && file.getName().endsWith(".class")) {
+                foundClass = true;
+                String pkg = "";
+                file = dir;
+                while (!file.equals(base)) {
+                    if (!"".equals(pkg)) {
+                        pkg = "." + pkg;
+                    }
+                    pkg = file.getName() + pkg;
+                    file = file.getParentFile();
+                }
+                if (!pkgs.contains(pkg)) {
+                    pkgs.add(pkg);
+                }
+            }
+        }
     }
 
     private static String getPackageName(String clzName) {
