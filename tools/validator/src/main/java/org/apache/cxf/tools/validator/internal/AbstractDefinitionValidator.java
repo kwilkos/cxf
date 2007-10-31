21693c81051addb2505b31207791a7693792b4a5
James Maode Mao <mmao@apache.org>
2007-10-31 08:03:08 +0000

    CXF-1057  * WsdlValidator support catalog  * Share the Bus among the toolsContainer  * Validator get the toolsContext from the container
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@590585 13f79535-47bb-0310-9956-ffa450edef68

--- tools/validator/src/main/java/org/apache/cxf/tools/validator/internal/AbstractDefinitionValidator.java
+++ tools/validator/src/main/java/org/apache/cxf/tools/validator/internal/AbstractDefinitionValidator.java
@@ -21,30 +21,45 @@ package org.apache.cxf.tools.validator.internal;
 
 import javax.wsdl.Definition;
 import javax.xml.stream.Location;
+
+import org.apache.cxf.Bus;
 import org.apache.cxf.tools.common.ToolContext;
 import org.apache.cxf.tools.validator.AbstractValidator;
 
 public abstract class AbstractDefinitionValidator extends AbstractValidator {
+    
     protected final Definition def;
     protected ToolContext env;
+    
+    private final Bus bus;
 
     public AbstractDefinitionValidator() {
         super();
         this.def = null;
+        this.bus = null;
     }
 
     public AbstractDefinitionValidator(final Definition definition) {
-        this.def = definition;
+        this(definition, null, null);
     }
 
-    public AbstractDefinitionValidator(final Definition definition, ToolContext pEnv) {
+    public AbstractDefinitionValidator(final Definition definition, final Bus b) {
+        this(definition, null, b);
+    }
+
+    public AbstractDefinitionValidator(final Definition definition, final ToolContext pEnv, final Bus b) {
         this.def = definition;
         this.env = pEnv;
+        this.bus = b;
     }
 
     public void addError(Location loc, String msg) {
         String errMsg = loc != null ? "line " + loc.getLineNumber() + " of " : "";
         errMsg = errMsg + def.getDocumentBaseURI() + " " + msg;
         addErrorMessage(errMsg);
+    }
+
+    public Bus getBus() {
+        return this.bus;
     }
 }
