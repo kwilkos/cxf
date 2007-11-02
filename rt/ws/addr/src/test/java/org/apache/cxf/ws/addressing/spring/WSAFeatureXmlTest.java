2e0bfc59720fb09da52d71eefcfbf517d66d10a6
J. Daniel Kulp <dkulp@apache.org>
2007-11-02 02:32:23 +0000

    Merged revisions 590916 via svnmerge from https://svn.apache.org/repos/asf/incubator/cxf/trunk
    ........
      r590916 | ningjiang | 2007-11-01 03:02:31 -0400 (Thu, 01 Nov 2007) | 1 line
      
      Added some tests on the WSAFeature, also added AbstractWSDLBasedEndpointFactory missing merges
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/branches/2.0.x-fixes@591196 13f79535-47bb-0310-9956-ffa450edef68

--- rt/ws/addr/src/test/java/org/apache/cxf/ws/addressing/spring/WSAFeatureXmlTest.java
+++ rt/ws/addr/src/test/java/org/apache/cxf/ws/addressing/spring/WSAFeatureXmlTest.java
@@ -18,17 +18,23 @@
  */
 package org.apache.cxf.ws.addressing.spring;
 
+import java.util.List;
+
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusException;
 import org.apache.cxf.bus.spring.SpringBusFactory;
 import org.apache.cxf.configuration.Configurer;
+import org.apache.cxf.endpoint.Client;
 import org.apache.cxf.endpoint.Endpoint;
 import org.apache.cxf.endpoint.Server;
+import org.apache.cxf.frontend.ClientProxy;
 import org.apache.cxf.interceptor.Interceptor;
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
 import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
 import org.apache.cxf.test.AbstractCXFTest;
 import org.apache.cxf.ws.addressing.MAPAggregator;
 import org.apache.cxf.ws.addressing.soap.MAPCodec;
+import org.apache.hello_world_soap_http.Greeter;
 import org.apache.hello_world_soap_http.GreeterImpl;
 import org.junit.Test;
 
@@ -36,27 +42,46 @@ public class WSAFeatureXmlTest extends AbstractCXFTest {
     
     @Override
     protected Bus createBus() throws BusException {
-        return new SpringBusFactory().createBus("/org/apache/cxf/ws/addressing/spring/server.xml");
+        return new SpringBusFactory().createBus("/org/apache/cxf/ws/addressing/spring/spring.xml");
     }
 
     @Test
     public void testServerFactory() {
         JaxWsServerFactoryBean sf = new JaxWsServerFactoryBean();
+     
+        assert bus != null;
         sf.setServiceBean(new GreeterImpl());
         sf.setAddress("http://localhost/test");
         sf.setStart(false);
-        sf.setBus(getBus());
         
         Configurer c = getBus().getExtension(Configurer.class);
-        c.configureBean("test", sf);
+        c.configureBean("server", sf);
         
         Server server = sf.create();
         
         Endpoint endpoint = server.getEndpoint();
+        checkAddressInterceptors(endpoint.getInInterceptors());
+    }
+    
+    @Test
+    public void testClientProxyFactory() {
+      
+        JaxWsProxyFactoryBean cf = new JaxWsProxyFactoryBean(); 
+        cf.setAddress("http://localhost/test");        
+        cf.setServiceClass(Greeter.class);
+        cf.setBus(getBus());
+        Configurer c = getBus().getExtension(Configurer.class);
+        c.configureBean("client.proxyFactory", cf);
+        Greeter greeter = (Greeter) cf.create();
+        Client client = ClientProxy.getClient(greeter);        
+        checkAddressInterceptors(client.getInInterceptors());
+    }
+    
+    private void checkAddressInterceptors(List<Interceptor> interceptors) {
         boolean hasAg = false;
         boolean hasCodec = false;
         
-        for (Interceptor i : endpoint.getInInterceptors()) {
+        for (Interceptor i : interceptors) {
             if (i instanceof MAPAggregator) {
                 hasAg = true;
             } else if (i instanceof MAPCodec) {
