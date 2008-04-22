9705fd84f360095655c3cd1bc8c706244b807b29
J. Daniel Kulp <dkulp@apache.org>
2008-04-22 22:33:38 +0000

    [CXF-1544] Fix compile errors.  Patch from Sergey B. applied.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cxf/trunk@650680 13f79535-47bb-0310-9956-ffa450edef68

--- distribution/src/main/release/samples/jax_rs/basic/src/demo/jaxrs/server/Server.java
+++ distribution/src/main/release/samples/jax_rs/basic/src/demo/jaxrs/server/Server.java
@@ -27,7 +27,8 @@ public class Server {
     protected Server() throws Exception {
         JAXRSServerFactoryBean sf = new JAXRSServerFactoryBean();
         sf.setResourceClasses(CustomerService.class);
-        sf.setResourceProvider(CustomerService.class, new SingletonResourceProvider());
+        sf.setResourceProvider(CustomerService.class, 
+            new SingletonResourceProvider(new CustomerService()));
         sf.setAddress("http://localhost:9000/");
 
         sf.create();
