72f0d9e1632f9ab959e256c820c8f7c91b23e8c2
J. Daniel Kulp <dkulp@apache.org>
2008-05-29 17:32:29 +0000

    Merged revisions 661409,661413 via svnmerge from https://svn.apache.org/repos/asf/cxf/trunk
    ........
      r661409 | dkulp | 2008-05-29 13:23:34 -0400 (Thu, 29 May 2008) | 2 lines
      
      [CXF-1602] Add better fault message 
    ........
      r661413 | dkulp | 2008-05-29 13:30:29 -0400 (Thu, 29 May 2008) | 2 lines
      
      [CXF-1602] Revert last commit as null is actually valid if the date is a optional attribute.  Just return null.
    ........
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.0.x-fixes@661415 13f79535-47bb-0310-9956-ffa450edef68

--- tools/common/src/main/java/org/apache/cxf/tools/common/DataTypeAdapter.java
+++ tools/common/src/main/java/org/apache/cxf/tools/common/DataTypeAdapter.java
@@ -20,34 +20,54 @@ package org.apache.cxf.tools.common;
 
 import java.util.Calendar;
 import java.util.Date;
+
 import javax.xml.bind.DatatypeConverter;
 
 public final class DataTypeAdapter {
+
     private DataTypeAdapter() {
     }
 
     public static Date parseDate(String s) {
+        if (s == null) {
+            return null;
+        }
         return DatatypeConverter.parseDate(s).getTime();
     }
     public static String printDate(Date dt) {
+        if (dt == null) {
+            return null;
+        }
         Calendar c = Calendar.getInstance();
         c.setTime(dt);
         return DatatypeConverter.printDate(c);
     }
 
     public static Date parseTime(String s) {
+        if (s == null) {
+            return null;
+        }
         return DatatypeConverter.parseTime(s).getTime();
     }
     public static String printTime(Date dt) {
+        if (dt == null) {
+            return null;
+        }
         Calendar c = Calendar.getInstance();
         c.setTime(dt);
         return DatatypeConverter.printTime(c);
     }
 
     public static Date parseDateTime(String s) {
+        if (s == null) {
+            return null;
+        }
         return DatatypeConverter.parseDateTime(s).getTime();
     }
     public static String printDateTime(Date dt) {
+        if (dt == null) {
+            return null;
+        }
         Calendar c = Calendar.getInstance();
         c.setTime(dt);
         return DatatypeConverter.printDateTime(c);
