4193b298050a4f742a4782e418cd8b026bb4b5c7
Freeman Yue Fang <ffang@apache.org>
2008-08-06 10:42:46 +0000

    [CXF-1652]CXF Decoupled Endpoints are not getting cleaned up properly even after bus.Shutdown() in org.apache.cxf.systest.ws.rm.SequenceTest.
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.0.x-fixes@683216 13f79535-47bb-0310-9956-ffa450edef68

--- rt/transports/http/src/main/java/org/apache/cxf/transport/http/HTTPConduit.java
+++ rt/transports/http/src/main/java/org/apache/cxf/transport/http/HTTPConduit.java
@@ -921,6 +921,9 @@ public class HTTPConduit
         if (--decoupledDestinationRefCount == 0) {
             LOG.log(Level.INFO, "shutting down decoupled destination");
             decoupledDestination.shutdown();
+            
+            //this way we can release the port of decoupled destination
+            decoupledDestination.setMessageObserver(null);
         }
     }
     
