63b0f36d49e707becb1b48b64af4f5915dbcd419
J. Daniel Kulp <dkulp@apache.org>
2008-09-03 18:48:50 +0000

    Merged revisions 691715 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.1.x-fixes
    ................
      r691715 | dkulp | 2008-09-03 14:23:05 -0400 (Wed, 03 Sep 2008) | 12 lines
      
      Merged revisions 691706 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/trunk
      
      ........
        r691706 | dkulp | 2008-09-03 14:13:05 -0400 (Wed, 03 Sep 2008) | 5 lines
        
        [CXF-1778, CXF-1781] Fix memory leak with ws-addressing if server is down
        Make sure we process the mustUnderstand headers
        If List<Header> contains SoapHeader objects, make sure we write the mustUnderstand/actor stuff out
        Remove dependency on stax-utils
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.0.x-fixes@691729 13f79535-47bb-0310-9956-ffa450edef68

--- rt/databinding/jaxb/src/main/java/org/apache/cxf/jaxb/JAXBEncoderDecoder.java
+++ rt/databinding/jaxb/src/main/java/org/apache/cxf/jaxb/JAXBEncoderDecoder.java
@@ -192,6 +192,8 @@ public final class JAXBEncoderDecoder {
                     && part.getTypeQName().getLocalPart().equals("hexBinary")) {
                     mObj = new HexBinaryAdapter().marshal((byte[])mObj);
                     writeObject(u, source, new JAXBElement(elName, String.class, mObj));
+                } else if (mObj instanceof JAXBElement) {
+                    writeObject(marshaller, source, mObj);
                 } else {
                     writeObject(u, source, new JAXBElement(elName, cls, mObj));
                 }
