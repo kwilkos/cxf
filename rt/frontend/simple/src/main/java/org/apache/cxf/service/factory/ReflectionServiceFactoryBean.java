102535a8051f9e62eb73c9d36c6b6979f1f33868
J. Daniel Kulp <dkulp@apache.org>
2008-09-12 01:23:20 +0000

    Merged revisions 694467 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.1.x-fixes
    ................
      r694467 | dkulp | 2008-09-11 16:43:50 -0400 (Thu, 11 Sep 2008) | 9 lines
      
      Merged revisions 693819 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/trunk
      
      ........
        r693819 | seanoc | 2008-09-10 08:44:01 -0400 (Wed, 10 Sep 2008) | 1 line
        
        Modified code to remove use of NodeList
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.0.x-fixes@694574 13f79535-47bb-0310-9956-ffa450edef68

--- rt/frontend/simple/src/main/java/org/apache/cxf/service/factory/ReflectionServiceFactoryBean.java
+++ rt/frontend/simple/src/main/java/org/apache/cxf/service/factory/ReflectionServiceFactoryBean.java
@@ -278,7 +278,9 @@ public class ReflectionServiceFactoryBean extends AbstractServiceFactoryBean {
     }
     
     protected void buildServiceFromWSDL(String url) {
-        LOG.info("Creating Service " + getServiceQName() + " from WSDL: " + url);
+        if (LOG.isLoggable(Level.FINE)) {
+            LOG.fine("Creating Service " + getServiceQName() + " from WSDL: " + url);
+        }
         populateFromClass = false;
         WSDLServiceFactory factory = new WSDLServiceFactory(getBus(), url, getServiceQName());
         setService(factory.create());
@@ -299,7 +301,9 @@ public class ReflectionServiceFactoryBean extends AbstractServiceFactoryBean {
     }
 
     protected void buildServiceFromClass() {
-        LOG.info("Creating Service " + getServiceQName() + " from class " + getServiceClass().getName());
+        if (LOG.isLoggable(Level.FINE)) {
+            LOG.fine("Creating Service " + getServiceQName() + " from class " + getServiceClass().getName());
+        }
         populateFromClass = true;
 
         if (Proxy.isProxyClass(this.getServiceClass())) {
