77cd418bc01a2dd04a8fa21f063f8954f7ac3ed0
Willem Ning Jiang <ningjiang@apache.org>
2008-10-31 06:44:58 +0000

    Merged revisions 709357 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.1.x-fixes
    ................
      r709357 | ningjiang | 2008-10-31 14:10:08 +0800 (Fri, 31 Oct 2008) | 13 lines
      
      Merged revisions 709353-709354 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/trunk
      
      ........
        r709353 | ningjiang | 2008-10-31 13:35:50 +0800 (Fri, 31 Oct 2008) | 1 line
        
        CXF-1749 quick fix for JMS Encoding issue
      ........
        r709354 | ningjiang | 2008-10-31 13:49:18 +0800 (Fri, 31 Oct 2008) | 1 line
        
        CXF-1749 update the messages.properties
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.0.x-fixes@709363 13f79535-47bb-0310-9956-ffa450edef68

--- rt/transports/jms/src/test/java/org/apache/cxf/transport/jms/JMSDestinationTest.java
+++ rt/transports/jms/src/test/java/org/apache/cxf/transport/jms/JMSDestinationTest.java
@@ -199,6 +199,7 @@ public class JMSDestinationTest extends AbstractJMSTester {
         header.setJMSPriority(1);
         header.setTimeToLive(1000);
         outMessage.put(JMSConstants.JMS_CLIENT_REQUEST_HEADERS, header);
+        outMessage.put(Message.ENCODING, "US-ASCII");
     }
 
     private void verifyReceivedMessage(Message inMessage) {
@@ -211,12 +212,16 @@ public class JMSDestinationTest extends AbstractJMSTester {
             ex.printStackTrace();
         }
         String reponse = new String(bytes);
-        assertEquals("The reponse date should be equals", reponse, "HelloWorld");
+        assertEquals("The reponse date should be equal", reponse, "HelloWorld");
     }
 
     private void verifyRequestResponseHeaders(Message inMessage, Message outMessage) {
         JMSMessageHeadersType outHeader = (JMSMessageHeadersType)outMessage
             .get(JMSConstants.JMS_CLIENT_REQUEST_HEADERS);
+        String inEncoding = (String) inMessage.get(Message.ENCODING);
+        String outEncoding = (String) outMessage.get(Message.ENCODING);
+        
+        assertEquals("The message encoding should be equal", inEncoding, outEncoding);
 
         JMSMessageHeadersType inHeader = (JMSMessageHeadersType)inMessage
             .get(JMSConstants.JMS_CLIENT_RESPONSE_HEADERS);
@@ -337,6 +342,8 @@ public class JMSDestinationTest extends AbstractJMSTester {
                     backConduit = destination.getBackChannel(m, null, null);
                     // wait for the message to be got from the conduit
                     Message replyMessage = new MessageImpl();
+                    // copy the message encoding
+                    replyMessage.put(Message.ENCODING, m.get(Message.ENCODING));
                     sendoutMessage(backConduit, replyMessage, true);
                 } catch (Exception e) {
                     throw new RuntimeException(e);
