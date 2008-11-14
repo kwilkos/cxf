2035cd4ac91805ffa46fd6978a8777915714d217
J. Daniel Kulp <dkulp@apache.org>
2008-11-14 22:21:54 +0000

    Fix checkstyle error
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.0.x-fixes@714164 13f79535-47bb-0310-9956-ffa450edef68

--- common/common/src/main/java/org/apache/cxf/staxutils/W3CDOMStreamReader.java
+++ common/common/src/main/java/org/apache/cxf/staxutils/W3CDOMStreamReader.java
@@ -30,7 +30,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
-import org.w3c.dom.Text;
 
 import org.apache.cxf.helpers.DOMUtils;
 import org.apache.cxf.staxutils.AbstractDOMStreamReader.ElementFrame;
