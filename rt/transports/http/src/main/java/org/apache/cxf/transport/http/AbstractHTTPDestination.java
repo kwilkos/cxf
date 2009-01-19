8e65f173841f4078308feadc09ea0a492ab032ca
J. Daniel Kulp <dkulp@apache.org>
2009-01-19 20:45:30 +0000

    Merged revisions 727794 via svnmerge from https://svn.apache.org/repos/asf/cxf/branches/2.1.x-fixes
    ................
      r727794 | dkulp | 2008-12-18 14:18:05 -0500 (Thu, 18 Dec 2008) | 9 lines
      
      Merged revisions 727445 via svnmerge from 
      https://svn.apache.org/repos/asf/cxf/trunk
      
      ........
        r727445 | dkulp | 2008-12-17 12:17:28 -0500 (Wed, 17 Dec 2008) | 2 lines
        
        Add ability to configure keep-alive params
      ........
    ................
    
    
    git-svn-id: https://svn.apache.org/repos/asf/cxf/branches/2.0.x-fixes@735801 13f79535-47bb-0310-9956-ffa450edef68

--- rt/transports/http/src/main/java/org/apache/cxf/transport/http/AbstractHTTPDestination.java
+++ rt/transports/http/src/main/java/org/apache/cxf/transport/http/AbstractHTTPDestination.java
@@ -395,6 +395,8 @@ public abstract class AbstractHTTPDestination extends AbstractMultiplexDestinati
         if (policy.isSetHonorKeepAlive() && !policy.isHonorKeepAlive()) {
             headers.put("Connection",
                         createMutableList("close"));
+        } else if (policy.isSetKeepAliveParameters()) {
+            headers.put("Keep-Alive", createMutableList(policy.getKeepAliveParameters()));
         }
         
     
